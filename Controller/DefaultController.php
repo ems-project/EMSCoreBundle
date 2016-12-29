83c400ebcab940178e92dbe5dc414c596bf8f294
theus deka <me@theus.be>
2016-12-29 01:02:32 +0100

    Some debug during migration

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -8,19 +8,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        
-    	return $this->redirectToRoute('notifications.inbox');
-    	
-//         return $this->render('EMSCoreBundle:default:index.html.twig', [
-//             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-//         ]);
-    }
     
     /**
      * @Route("/documentation", name="documentation")

ba1080332f603c132c1ae90e1364e273ce1b03ec
theus deka <me@theus.be>
2017-07-03 13:41:04 +0200

    Hidden Filed Type

--- Form/DataField/HiddenFieldType.php
+++ Form/DataField/HiddenFieldType.php
@@ -4,7 +4,9 @@ namespace EMS\CoreBundle\Form\DataField;
 
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
-				
+use EMS\CoreBundle\Entity\DataField;
+use EMS\CoreBundle\Entity\FieldType;
+						
 /**
  * Defined a Container content type.
  * It's used to logically groups subfields together. However a Container is invisible in Elastic search.
@@ -13,15 +15,15 @@ use Symfony\Component\Form\FormBuilderInterface;
  *        
  */
  class HiddenFieldType extends DataFieldType {
-	
-
+	//TODO: deorecated class?
+ 	
  	/**
  	 *
  	 * {@inheritdoc}
  	 *
  	 */
  	public function getLabel(){
- 		throw new \Exception('This Field Type should not used as field (as service)');
+ 		throw new \Exception('This Field Type should not be used as field (as service)');
  	}
  	
 	/**
@@ -30,7 +32,44 @@ use Symfony\Component\Form\FormBuilderInterface;
 	 *
 	 */
 	public function buildForm(FormBuilderInterface $builder, array $options) {
-			$builder->add ( 'encodedText', HiddenType::class);		
+			$builder->add ( 'value', HiddenType::class);		
+	}
+	
+	/**
+	 *
+	 * {@inheritDoc}
+	 * @see \EMS\CoreBundle\Form\DataField\DataFieldType::getBlockPrefix()
+	 */
+	public function getBlockPrefix() {
+		return 'empty';
+	}
+	
+	/**
+	 *
+	 * {@inheritDoc}
+	 * @see \EMS\CoreBundle\Form\DataField\DataFieldType::viewTransform()
+	 */
+	public function viewTransform(DataField $dataField) {
+		$out = parent::viewTransform($dataField);
+		return ['value' => json_encode($out)];
+	}
+	
+	/**
+	 *
+	 * {@inheritDoc}
+	 * @see \EMS\CoreBundle\Form\DataField\DataFieldType::reverseViewTransform()
+	 */
+	public function reverseViewTransform($data, FieldType $fieldType) {
+		$dataField = parent::reverseViewTransform($value, $fieldType);
+		try {
+			$value = json_decode($data['value']);
+			$dataField->setRawData($value);
+		}
+		catch(\Exception $e) {
+			$dataField->setRawData(null);
+			$dataField->addMessage('ems was not able to parse the field');
+		}
+		return $dataField;
 	}
 
 }

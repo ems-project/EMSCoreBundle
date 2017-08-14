2466bace8a9c263c5ce836da3ac273543d2668f0
theus deka <me@theus.be>
2017-08-14 11:17:47 +0200

    Helper service

---
+++ Service/HelperService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace EMS\CoreBundle\Service;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Symfony\Component\HttpFoundation\RequestStack;
+use EMS\CoreBundle\Helper\PagingTool;
+
+class HelperService {
+	/**@var Registry $doctrine */
+	protected $doctrine;
+	
+	
+	public function __construct(Registry $doctrine, RequestStack $requestStack, $paging_size){
+		$this->doctrine = $doctrine;
+		$this->paging_size = $paging_size;
+		$this->requestStack = $requestStack;
+	}
+	
+	public function getPagingTool($entityName, $route, $defaultOrderField) {
+		return new PagingTool($this->requestStack->getCurrentRequest(), $this->doctrine->getRepository($entityName), $route, $defaultOrderField, $this->paging_size);
+	}
+}

4d05c8e0b019b2b31745ed2807a966a6e59ea8e8
theus deka <me@theus.be>
2018-03-17 19:56:59 +0100

    AggregateOPtion and SearchOption entities, repositories and services

---
+++ Service/AggregateOptionService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace EMS\CoreBundle\Service;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use EMS\CoreBundle\Entity\WysiwygProfile;
+use EMS\CoreBundle\Repository\WysiwygProfileRepository;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class AggregateOptionService{
+	/**@var Registry $doctrine */
+	private $doctrine;
+	/**@var Session $session*/
+	private $session;
+	/**@var TranslatorInterface $translator */
+	private $translator;
+	
+	public function __construct(Registry $doctrine, Session $session, TranslatorInterface $translator) {
+		$this->doctrine = $doctrine;
+		$this->session = $session;
+		$this->translator= $translator;
+	}
+}

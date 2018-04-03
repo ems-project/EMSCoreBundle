ed2153899043f5d38a66cabeb8a1612c98fcb056
David Mattei <david.mattei@smals.be>
2018-04-03 16:40:01 +0200

    issue-40 collapsible collection

--- Resources/public/js/app.js
+++ Resources/public/js/app.js
@@ -79,6 +79,29 @@ $(document).ready(function() {
 	    $(this).html(toggleTex);
 		$(this).data('toggle-contain', text);
 	});
+
+    $('.collapsible-collection')
+        .on('click', '.button-collapse', function() {
+            var $isExpanded = ($(this).attr('aria-expanded') === 'true');
+            $(this).parent().find('button').attr('aria-expanded', !$isExpanded);
+
+            $panel = $(this).closest('.panel');
+            $panel.find('.collapse').first().collapse('toggle');
+        })
+        .on('click', '.button-collapse-all', function() {
+            var $isExpanded = ($(this).attr('aria-expanded') === 'true');
+            $(this).parent().find('button').attr('aria-expanded', !$isExpanded);
+
+            $panel = $(this).closest('.panel');
+            $panel.find('.button-collapse').attr('aria-expanded', !$isExpanded);
+            $panel.find('.button-collapse-all').attr('aria-expanded', !$isExpanded);
+
+            if (!$isExpanded) {
+                $panel.find('.collapse').collapse('show');
+            } else {
+                $panel.find('.collapse').collapse('hide');
+            }
+        });
 	
 });
 

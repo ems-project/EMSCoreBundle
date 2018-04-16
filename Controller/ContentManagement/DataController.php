516b6bd8dbaa08101c893cd960939704b8bb0be8
Mathieu De Keyzer <mathieu@dekeyzer.net>
2018-04-16 10:04:33 +0200

    debug

--- Controller/ContentManagement/DataController.php
+++ Controller/ContentManagement/DataController.php
@@ -34,7 +34,6 @@ use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -657,7 +656,7 @@ class DataController extends AppController
             throw new NotFoundHttpException('View type not found');
         }
 
-        /** @var \EMS\CoreBundle\Form\View\ViewType $viewType */
+        /** @var ViewType $viewType */
         $viewType = $this->get($view->getType());
 
         return $viewType->generateResponse($view, $request);

28474cf2577a8fedde0539ded6593ed30607b184
theus deka <me@theus.be>
2018-08-25 12:40:39 +0200

    Reformated code

--- Service/Storage/FileSystemStorage.php
+++ Service/Storage/FileSystemStorage.php
@@ -3,74 +3,81 @@
 namespace EMS\CoreBundle\Service\Storage;
 
 
-
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 use function file_exists;
 use function filesize;
 use function fopen;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Finder\Finder;
 use function unlink;
 
-class FileSystemStorage implements StorageInterface {
-	
-	private $storagePath;
-	
-	public function __construct($storagePath) {
-		$this->storagePath = $storagePath;
-	}
+class FileSystemStorage implements StorageInterface
+{
 
-	private function getPath($hash, $cacheContext=null){
-		if(!file_exists($this->storagePath)){
-			mkdir($this->storagePath, 0777, true);
-		}
+    private $storagePath;
 
-		$out = $this->storagePath;
-		if($cacheContext) {
-			$out .= DIRECTORY_SEPARATOR.'cache'.DIRECTORY_SEPARATOR.$cacheContext;
-		}
-		$out.= DIRECTORY_SEPARATOR.substr($hash, 0, 3);
-		
-		
-		if(!file_exists($out) ) {
-			mkdir($out, 0777, true);
-		}
-		
-		return $out.DIRECTORY_SEPARATOR.$hash;
-	}
-	
-	public function head($hash, $cacheContext=false) {
-		return file_exists($this->getPath($hash, $cacheContext));
-	}
-	
-	public function create($hash, $filename, $cacheContext=false){
-		return copy($filename, $this->getPath($hash, $cacheContext));
-	}
-	
-	public function supportCacheStore() {
-		return true;
-	}
+    public function __construct($storagePath)
+    {
+        $this->storagePath = $storagePath;
+    }
 
-	public function read($hash, $cacheContext=false){
-		$out = $this->getPath($hash, $cacheContext);
-		if(!file_exists($out)){
-			return false;
-		}
+    public function head($hash, $cacheContext = false)
+    {
+        return file_exists($this->getPath($hash, $cacheContext));
+    }
 
-		return fopen($out, 'rb');
-	}
-	
-	public function getLastUpdateDate($hash, $cacheContext=false){
-		$path = $this->getPath($hash, $cacheContext);
-		if( file_exists($path)) {
-			return @filemtime($path);
-		}
-		return false;
-	}
+    private function getPath($hash, $cacheContext = null)
+    {
+        if (!file_exists($this->storagePath)) {
+            mkdir($this->storagePath, 0777, true);
+        }
 
-	public function getSize($hash, $cacheContext = false)
+        $out = $this->storagePath;
+        if ($cacheContext) {
+            $out .= DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR . $cacheContext;
+        }
+        $out .= DIRECTORY_SEPARATOR . substr($hash, 0, 3);
+
+
+        if (!file_exists($out)) {
+            mkdir($out, 0777, true);
+        }
+
+        return $out . DIRECTORY_SEPARATOR . $hash;
+    }
+
+    public function create($hash, $filename, $cacheContext = false)
+    {
+        return copy($filename, $this->getPath($hash, $cacheContext));
+    }
+
+    public function supportCacheStore()
+    {
+        return true;
+    }
+
+    public function read($hash, $cacheContext = false)
+    {
+        $out = $this->getPath($hash, $cacheContext);
+        if (!file_exists($out)) {
+            return false;
+        }
+
+        return fopen($out, 'rb');
+    }
+
+    public function getLastUpdateDate($hash, $cacheContext = false)
     {
         $path = $this->getPath($hash, $cacheContext);
-        if( file_exists($path)) {
+        if (file_exists($path)) {
+            return @filemtime($path);
+        }
+        return false;
+    }
+
+    public function getSize($hash, $cacheContext = false)
+    {
+        $path = $this->getPath($hash, $cacheContext);
+        if (file_exists($path)) {
             return @filesize($path);
         }
         return false;
@@ -78,7 +85,7 @@ class FileSystemStorage implements StorageInterface {
 
     public function __toString()
     {
-        return FileSystemStorage::class." ($this->storagePath)";
+        return FileSystemStorage::class . " ($this->storagePath)";
     }
 
     /**
@@ -87,20 +94,20 @@ class FileSystemStorage implements StorageInterface {
     public function clearCache()
     {
         $fileSystem = new Filesystem();
-        $fileSystem->remove($this->storagePath.DIRECTORY_SEPARATOR.'cache');
+        $fileSystem->remove($this->storagePath . DIRECTORY_SEPARATOR . 'cache');
         return true;
     }
 
     public function remove($hash)
     {
         $file = $this->getPath($hash);
-        if(file_exists($file))
-        {
+        if (file_exists($file)) {
+
             unlink($file);
         }
         $finder = new Finder();
         $finder->name($hash);
-        foreach ($finder->in($this->storagePath.DIRECTORY_SEPARATOR.'cache') as $file) {
+        foreach ($finder->in($this->storagePath . DIRECTORY_SEPARATOR . 'cache') as $file) {
             unlink($file);
         }
         return true;

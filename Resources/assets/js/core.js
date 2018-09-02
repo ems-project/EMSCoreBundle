2b0250009a889eeec6069bf69e652a3e6846ddfa
theus deka <me@theus.be>
2018-09-02 04:04:27 +0200

    Migrated to yarn

--- Resources/assets/js/core.js
+++ Resources/assets/js/core.js
@@ -1,7 +1,28 @@
 const $ = require('jquery');
+const jQuery = require('jquery');
 window.$ = $;
 window.jQuery = $;
 
+require('webpack-jquery-ui');
+require('bootstrap');
+require('bootstrap-fileinput');
+require('select2');
+require('jquery.dataTables');
+// var dt      = require( 'datatables.net' )( window, $ );
+// require('datatables');
+// require('admin-lte');
+// require('ckeditor');
+// require('bootstrap-colorpicker');
+require('moment');
+require('daterangepicker');
+require('bootstrap-datepicker');
+require('bootstrap-select');
+require('bootstrap-timepicker');
+
 $(document).ready(function() {
     console.log('ems_core requirements have been defined');
+});
+
+jQuery(document).ready(function() {
+    console.log('ems_core requirements have been defined 2');
 });

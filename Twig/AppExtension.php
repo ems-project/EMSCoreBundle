0352568df7dd714fec06937cafaa5f35249c93bf
theus deka <me@theus.be>
2018-09-25 11:10:44 +0200

    Allow to generate asset path instead of be url with the twig filter internal_links

--- Twig/AppExtension.php
+++ Twig/AppExtension.php
@@ -627,7 +627,7 @@ class AppExtension extends \Twig_Extension
 		return $username;
 	}
 
-	function internalLinks($input){
+	function internalLinks($input, $fileName=false){
 		$url = $this->router->generate('data.link', ['key'=>'object:'], UrlGeneratorInterface::ABSOLUTE_PATH);
 		$out = preg_replace('/ems:\/\/object:/i', $url, $input);
 		
@@ -635,8 +635,11 @@ class AppExtension extends \Twig_Extension
 		$path = substr($path, 0, strlen($path)-8);
 		$out= preg_replace_callback(
 			'/(ems:\/\/asset:)([^\n\r"\'\?]*)/i',
-			function ($matches) use ($path) {
-					return $path.$matches[2];
+			function ($matches) use ($path, $fileName) {
+			    if($fileName){
+                    return $this->fileService->getFile($matches[2]);
+                }
+				return $path.$matches[2];
 			},
 			$out
 		); 

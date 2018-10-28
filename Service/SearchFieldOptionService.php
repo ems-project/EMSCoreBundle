c8eaa9f80d95a61380cb02720db3cce8e0bf37be
theus deka <me@theus.be>
2018-10-28 10:36:05 +0100

    Search field option (entity, service, repository and update script)

---
+++ Service/SearchFieldOptionService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace EMS\CoreBundle\Service;
+
+use EMS\CoreBundle\Entity\SortOption;
+
+class SearchFieldOptionService extends EntityService{
+	
+	protected function getRepositoryIdentifier() {
+		return 'EMSCoreBundle:SearchFieldOption';
+	}
+	
+	protected function getEntityName() {
+		return 'Search Field Option';
+	}
+}

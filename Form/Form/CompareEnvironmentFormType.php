f7f857da70bcc386c2e1246f621eb9a9067b3320
David Meert <david.meert@smals.be>
2019-03-07 15:44:56 +0100

    fix LF and indentations

--- Form/Form/CompareEnvironmentFormType.php
+++ Form/Form/CompareEnvironmentFormType.php
@@ -10,37 +10,39 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 
-class CompareEnvironmentFormType extends AbstractType {
-	/**
-	 *
-	 * {@inheritdoc}
-	 *
-	 */
-	public function buildForm(FormBuilderInterface $builder, array $options) {
-		// http://symfony.com/doc/current/cookbook/form/dynamic_form_modification.html#cookbook-dynamic-form-modification-suppressing-form-validation
-		$builder->addEventListener ( FormEvents::POST_SUBMIT, function (FormEvent $event) {
-			$event->stopPropagation ();
-		}, 900 );
-		
-		$builder
-			->add ( 'environment', EnvironmentPickerType::class, [
-					'inMyCircle' => true,
-					'managedOnly' => true,
-			] )
-			->add ( 'withEnvironment', EnvironmentPickerType::class, [
-					'inMyCircle' => true,
-					'managedOnly' => true,
-			] )
-			->add('contentTypes', ContentTypePickerType::class, [
-					'multiple' => true,
-					'required' => false,
-			])
-			->add('compare', SubmitEmsType::class, [
-				'attr' => [ 
-						'class' => 'btn-primary btn-md' 
-				],
-				'icon' => 'fa fa-columns'
-		]);
-	}
+class CompareEnvironmentFormType extends AbstractType
+{
+    /**
+     *
+     * {@inheritdoc}
+     *
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // http://symfony.com/doc/current/cookbook/form/dynamic_form_modification.html#cookbook-dynamic-form-modification-suppressing-form-validation
+        $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+            $event->stopPropagation();
+        }, 900);
+
+        $builder
+            ->add('environment', EnvironmentPickerType::class, [
+                'inMyCircle' => true,
+                'managedOnly' => true,
+            ])
+            ->add('withEnvironment', EnvironmentPickerType::class, [
+                'inMyCircle' => true,
+                'managedOnly' => true,
+            ])
+            ->add('contentTypes', ContentTypePickerType::class, [
+                'multiple' => true,
+                'required' => false,
+            ])
+            ->add('compare', SubmitEmsType::class, [
+                'attr' => [
+                    'class' => 'btn-primary btn-md'
+                ],
+                'icon' => 'fa fa-columns'
+            ]);
+    }
 
 }

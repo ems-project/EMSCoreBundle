6b8fad5cd85cbd038ab114d04ed9a87c8874a296
theus deka <me@theus.be>
2019-03-11 12:38:55 +0100

    Cleaning of the AssetController and activate autowiring of the core twig's extension

--- Controller/ContentManagement/AssetController.php
+++ Controller/ContentManagement/AssetController.php
@@ -2,10 +2,9 @@
 
 namespace EMS\CoreBundle\Controller\ContentManagement;
 
-use Elasticsearch\Client;
-use EMS\CommonBundle\Storage\Processor\Config;
+use EMS\CommonBundle\Helper\EmsConst;
 use EMS\CommonBundle\Storage\Processor\Processor;
-use EMS\CoreBundle\Service\ContentTypeService;
+use EMS\CoreBundle\Twig\AppExtension;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,22 +14,13 @@ class AssetController extends AbstractController
 {
     /** @var Processor */
     private $processor;
-    /** @var Client */
-    private $client;
-    /** @var ContentTypeService */
-    private $contentTypeService;
-    /** @var string */
-    private $configType;
-    /** @var string */
-    private $configIndex;
+    /** @var AppExtension */
+    private $appExtension;
 
-    public function __construct(Processor $processor, Client $client, ContentTypeService $contentTypeService, $configType, $configIndex)
+    public function __construct(Processor $processor, AppExtension $appExtension)
     {
         $this->processor = $processor;
-        $this->client = $client;
-        $this->contentTypeService = $contentTypeService;
-        $this->configType = $configType;
-        $this->configIndex = $configIndex;
+        $this->appExtension = $appExtension;
     }
 
     /**
@@ -49,55 +39,18 @@ class AssetController extends AbstractController
 
 
     /**
-	 * @Route("/asset/{processor}/{hash}", name="ems_asset_processor")
+     * @deprecated
+	 * @Route("/asset/{processor}/{hash}", name="ems_asset_processor", methods={"GET","HEAD"})
 	 */
 	public function assetProcessorAction(Request $request, string $processor, string $hash): Response
 	{
 	    @trigger_error(sprintf('The "%s::assetProcessorAction" controller is deprecated. Used "%s::assetAction" instead.', AssetController::class, AssetController::class), E_USER_DEPRECATED);
 
-        return $this->processor->createResponse($request, $processor, $hash, $this->getOptions($processor));
+	    return $this->redirect($this->appExtension->assetPath([
+	        EmsConst::CONTENT_FILE_HASH_FIELD => $hash,
+            EmsConst::CONTENT_FILE_NAME_FIELD => $request->query->get('name', 'filename'),
+            EmsConst::CONTENT_MIME_TYPE_FIELD => $request->query->get('type', 'application/octet-stream'),
+        ], $processor, [], 'emsco_asset_public'));
+
 	}
-
-	private function getOptions(string $processor): array
-    {
-        @trigger_error(sprintf('The "%s::getOptions" function is deprecated and should not be used anymore.', AssetController::class, AssetController::class), E_USER_DEPRECATED);
-
-        if (null == $this->configType) {
-            return [];
-        }
-
-        $contentType = $this->contentTypeService->getByName($this->configType);
-
-        if (!$contentType) {
-            return [];
-        }
-
-        try {
-            $result = $this->client->search([
-                'size' => 1,
-                'type' => $contentType->getName(),
-                'index' => $this->configIndex ? $this->configIndex : $contentType->getEnvironment()->getAlias(),
-                'body' => '{
-                   "query": {
-                      "term": {
-                         "_identifier": {
-                            "value": ' . json_encode($processor) . '
-                         }
-                      }
-                   }
-                }',
-            ]);
-
-            if ($result['hits']['total'] == 0) {
-                return [];
-            }
-
-            $defaults = Config::getDefaults();
-
-            // removes invalid options like _sha1, _finalized_by, ..
-            return array_intersect_key($result['hits']['hits'][0]['_source'] + $defaults, $defaults);
-        } catch (\Exception $e) {
-            return [];
-        }
-    }
 }

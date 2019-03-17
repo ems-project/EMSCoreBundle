e71c4f01c521a5403b95b1245a821d9e3320dce3
David Meert <david@davidmeert.be>
2019-03-18 00:25:07 +0100

    finish fixing ContentManagement Controllers

--- Controller/ContentManagement/ViewController.php
+++ Controller/ContentManagement/ViewController.php
@@ -17,6 +17,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ViewController extends AppController
 {

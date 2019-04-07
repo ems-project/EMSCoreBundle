6087acb51bae07002a13daa88de474524c3bf41b
theus deka <me@theus.be>
2019-04-07 07:26:43 +0200

    Refact of objectpicker

---
+++ Resources/assets/edit-revision.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import './js/core';
+import './css/app.scss';
+import './css/app.less';
+
+
+import EmsListeners from './js/EmsListeners';
+window.EmsListeners = EmsListeners;
+
+
--- Resources/assets/js/EmsListeners.js
+++ Resources/assets/js/EmsListeners.js
@@ -24,6 +24,7 @@ export default class EmsListeners {
         this.addNestedSortableListeners();
         this.addCodeEditorListeners();
         this.addRemoveButtonListeners();
+        this.addObjectPickerListeners();
     }
 
     static getAceConfig() {
@@ -109,7 +110,7 @@ export default class EmsListeners {
 
 
     addNestedSortableListeners() {
-        const nestedList =jquery(this.target).find('.nested-sortable');
+        const nestedList = jquery(this.target).find('.nested-sortable');
         nestedList.each(function() {
             const nestedList = jquery(this);
 
@@ -150,7 +151,7 @@ export default class EmsListeners {
             });
         });
 
-        jquery(this.target).find('.reorder-button').on('click', function(e){
+        jquery(this.target).find('.reorder-button').on('click', function(){
             const form = jquery(this).closest('form');
             const hierarchy = form.find('.nested-sortable').nestedSortable('toHierarchy', {startDepthCount: 0});
             form.find('input.reorder-items').val(JSON.stringify(hierarchy)).trigger("change");
@@ -197,6 +198,92 @@ export default class EmsListeners {
         });
     }
 
+
+    addObjectPickerListeners() {
+        const searchApiUrl = $('body').data('search-api');
+
+        jquery(this.target).find(".objectpicker").each(function(){
+            const selectItem = jquery(this);
+
+            const type = selectItem.data('type');
+            const searchId = selectItem.data('search-id');
+            const circleOnly = selectItem.data('circleOnly');
+            const dynamicLoading = selectItem.data('dynamic-loading');
+            const sortable = selectItem.data('sortable');
+
+            const params = {
+                escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
+                templateResult: formatRepo, // omitted for brevity, see the source of this page
+                templateSelection: formatRepoSelection, // omitted for brevity, see the source of this page
+                allowClear: true,
+                //https://github.com/select2/select2/issues/3781
+                placeholder: 'Select a document'
+            };
+
+            if(selectItem.attr('multiple')) {
+                params.closeOnSelect = false;
+            }
+
+            if(dynamicLoading){
+                params.minimumInputLength = 1;
+                params.ajax = {
+                    url: searchApiUrl,
+                    dataType: 'json',
+                    delay: 250,
+                    data: function (params) {
+                        return {
+                            q: params.term, // search term
+                            page: params.page,
+                            type: type,
+                            searchId: searchId,
+                            circle: circleOnly !== undefined
+                        };
+                    },
+                    processResults: function (data, params) {
+                        // parse the results into the format expected by Select2
+                        // since we are using custom formatting functions we do not need to
+                        // alter the remote JSON data, except to indicate that infinite
+                        // scrolling can be used
+                        params.page = params.page || 1;
+
+                        return {
+                            results: data.items,
+                            pagination: {
+                                more: (params.page * 30) < data.total_count
+                            }
+                        };
+                    },
+                    cache: true
+                };
+            }
+
+            selectItem.select2(params);
+
+            if(sortable){
+                selectItem.parent().find("ul.select2-selection__rendered").sortable({
+                    stop: function( ) {
+
+                        //http://stackoverflow.com/questions/45888/what-is-the-most-efficient-way-to-sort-an-html-selects-options-by-value-while
+                        const selected = selectItem.val();
+                        const my_options = selectItem.find("option");
+
+                        const ul = $(this);
+
+                        my_options.sort(function(a,b) {
+                            const indexA = ul.find("li[title='"+a.title.replace(/\'/g, "\\\'")+"']").index();
+                            const indexB = ul.find("li[title='"+b.title.replace(/\'/g, "\\\'")+"']").index();
+
+                            if (indexA > indexB) return 1;
+                            if (indexA < indexB) return -1;
+                            return 0
+                        });
+                        selectItem.empty().append( my_options );
+                        selectItem.val(selected);
+                    }
+                });
+            }
+        });
+    }
 
     addSelect2Listeners() {
         //Initialize Select2 Elements

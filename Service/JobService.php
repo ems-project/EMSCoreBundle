d9d5ef16cc477be45ed06e1bfc4e599b625c106d
theus deka <me@theus.be>
2019-12-16 16:02:25 +0100

    Twig controller action to list jobs (commands)

--- Service/JobService.php
+++ Service/JobService.php
@@ -67,6 +67,16 @@ class JobService
         $this->em->flush();
     }
 
+    public function findByUser($user)
+    {
+        $doneJobs = $this->repository->findBy([
+            'user' => $user,
+        ], [
+            'created' => 'DESC',
+        ], 20);
+        return $doneJobs;
+    }
+
     /**
      * @return int
      */

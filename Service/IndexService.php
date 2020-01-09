d739cf3b55c842049cded7f6b884f94616ed7a9e
laurensmertens <laurensmertens@users.noreply.github.com>
2020-01-09 10:54:24 +0100

    Remove orphaned indexes command (#257)
    Refactoring of Index Controller to reuse in the command line

---
+++ Service/IndexService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace EMS\CoreBundle\Service;
+
+use Elasticsearch\Client;
+use Psr\Log\LoggerInterface;
+
+final class IndexService
+{
+    /** @var AliasService */
+    private $aliasService;
+    /** @var Client */
+    private $client;
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(AliasService $aliasService, Client $client, LoggerInterface $logger)
+    {
+        $this->aliasService = $aliasService;
+        $this->client = $client;
+        $this->logger = $logger;
+    }
+
+    public function deleteOrphanIndexes(): void
+    {
+        $this->aliasService->build();
+        foreach ($this->aliasService->getOrphanIndexes() as $index) {
+            try {
+                $this->client->indices()->delete([
+                    'index' => $index['name'],
+                ]);
+                $this->logger->notice('log.index.delete_orphan_index', [
+                    'index_name' => $index['name'],
+                ]);
+            } catch (\RuntimeException $e) {
+                $this->logger->notice('log.index.index_not_found', [
+                    'index_name' => $index['name'],
+                ]);
+            }
+        }
+    }
+}

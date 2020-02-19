0016d6b4a817e38991a0e9411344f74a5a760b64
Jérôme Coppée <j.coppee@gmail.com>
2020-02-19 11:58:34 +0100

    refactor(TransformContentTypeCommand): add initialize and interact methods

--- Command/TransformContentTypeCommand.php
+++ Command/TransformContentTypeCommand.php
@@ -9,10 +9,14 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class TransformContentTypeCommand extends EmsCommand
 {
+    protected static $defaultName = 'ems:contenttype:transform';
+
     /** @var LoggerInterface */
     protected $logger;
 
@@ -22,7 +26,11 @@ class TransformContentTypeCommand extends EmsCommand
     /** @var TransformContentTypeService */
     protected $transformContentTypeService;
 
-    protected static $defaultName = 'ems:contenttype:transform';
+    /** @var SymfonyStyle */
+    private $io;
+
+    const ARGUMENT_CONTENTTYPE_NAME = 'contentTypeName';
+    const OPTION_STRICT = 'strict';
 
     public function __construct(LoggerInterface $logger, Client $client, ContentTypeService $contentTypeService, TransformContentTypeService $transformContentTypeService)
     {
@@ -38,42 +46,83 @@ class TransformContentTypeCommand extends EmsCommand
         $this
             ->setDescription('Transform the Content Type defined')
             ->addArgument(
-                'contentTypeName',
+                self::ARGUMENT_CONTENTTYPE_NAME,
                 InputArgument::REQUIRED,
                 'Content Type name'
+            )
+            ->addOption(
+                self::OPTION_STRICT,
+                null,
+                InputOption::VALUE_NONE,
+                'If set, the check failed will throw an exception'
             )
         ;
     }
 
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $this->io = new SymfonyStyle($input, $output);
+        $this->io->title('Transform content-type');
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output): void
+    {
+        $this->logger->info('Interact with TransformContentType command');
+
+        $this->io->section('Check environment name argument');
+        $this->checkContentTypeNameArgument($input);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->logger->info('Execute the TransformContentType command');
+
         $contentTypeName = $input->getArgument('contentTypeName');
         $contentType = $this->contentTypeService->getByName($contentTypeName);
-
-        if (!$contentType) {
-            $output->writeln('<error>Content type ' . $contentTypeName . ' not found</error>');
-            return -1;
-        }
 
         $total = $this->transformContentTypeService->getTotal($contentType);
         $hits = $this->transformContentTypeService->transform($contentType);
 
-        $output->writeln('<comment>Start transformation of ' . $contentType->getPluralName() . '</comment>');
-        $output->writeln('');
+        $this->io->note(\sprintf('Start transformation of "%s"', $contentType->getPluralName()));
 
-        $progress = new ProgressBar($output, $total);
-        $progress->start();
-
+        $this->io->progressStart($total);
         foreach ($hits as $hit) {
-            $progress->advance();
+            $this->io->progressAdvance();
+        }
+        $this->io->progressFinish();
+
+        $this->io->success(\sprintf('Transformation of "%s" content type done', $contentType->getPluralName()));
+        return 0;
+    }
+
+    private function checkContentTypeNameArgument(InputInterface $input)
+    {
+        $contentTypeName = $input->getArgument(self::ARGUMENT_CONTENTTYPE_NAME);
+        if (null === $contentTypeName) {
+            $message = 'The content type name is not provided';
+            $this->setContentTypeNameArgument($input, $message);
+            $this->checkContentTypeNameArgument($input);
+            return;
         }
 
-        $progress->finish();
+        $contentType = $this->contentTypeService->getByName($contentTypeName);
+        if (false === $contentType) {
+            $message = \sprintf('The content type "%s" not found', $contentTypeName);
+            $this->setContentTypeNameArgument($input, $message);
+            $this->checkContentTypeNameArgument($input);
+            return;
+        }
+    }
 
-        $output->writeln('');
-        $output->writeln('');
-        $output->writeln('<comment>Transformation done</comment>');
+    private function setContentTypeNameArgument(InputInterface $input, string $message): void
+    {
+        if ($input->getOption(self::OPTION_STRICT)) {
+            $this->logger->error($message);
+            throw new \Exception($message);
+        }
 
-        return 0;
+        $this->io->caution($message);
+        $contentTypeName = $this->io->choice('Select an existing content type', $this->contentTypeService->getAllNames());
+        $input->setArgument(self::ARGUMENT_CONTENTTYPE_NAME, $contentTypeName);
     }
 }

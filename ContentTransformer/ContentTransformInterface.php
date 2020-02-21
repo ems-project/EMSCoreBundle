e6031e88aac611b46345d93f7b31c97987e94493
Jérôme Coppée <j.coppee@gmail.com>
2020-02-21 16:46:00 +0100

    feature(ContentTransformer): add Context object argument in all methods [@dameert review]

--- ContentTransformer/ContentTransformInterface.php
+++ ContentTransformer/ContentTransformInterface.php
@@ -5,6 +5,6 @@ namespace EMS\CoreBundle\ContentTransformer;
 interface ContentTransformInterface
 {
     public function canTransform(ContentTransformContext $contentTransformContext): bool;
-    public function transform(string $input): string;
-    public function changed(string $output): bool;
+    public function transform(ContentTransformContext $contentTransformContext): string;
+    public function changed(ContentTransformContext $contentTransformContext): bool;
 }

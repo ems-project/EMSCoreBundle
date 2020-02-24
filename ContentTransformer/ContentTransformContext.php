96f590969dd3672e958dbd6546859a3b8ff9c771
Jérôme Coppée <j.coppee@gmail.com>
2020-02-24 11:57:07 +0100

    refactor(ContentTransformer): add "changed check" logic cfr. @dameert review

--- ContentTransformer/ContentTransformContext.php
+++ ContentTransformer/ContentTransformContext.php
@@ -25,6 +25,11 @@ class ContentTransformContext
         return $context;
     }
 
+    public function hasChanges(): bool
+    {
+        return $this->data !== $this->transformedData;
+    }
+
     public function getDataFieldType(): string
     {
         return $this->dataFieldType;

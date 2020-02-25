7fe6074af12552704e9eff9ab05b55a1deb5a183
theus deka <me@theus.be>
2020-02-25 21:23:02 +0100

    Customizable rest client's timeout

--- Service/RestClientService.php
+++ Service/RestClientService.php
@@ -12,11 +12,11 @@ class RestClientService
      * @param string $baseUrl
      * @return \GuzzleHttp\Client
      */
-    public function getClient($baseUrl = null)
+    public function getClient($baseUrl = null, int $timeout = 30)
     {
         $options = [
             // You can set any number of default request options.
-            'timeout'  => 30,
+            'timeout'  => $timeout,
         ];
         if ($baseUrl) {
             // Base URI is used with relative requests

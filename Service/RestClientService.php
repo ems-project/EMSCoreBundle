4a4edfa618d6208e693f323d7122fe9e7ba2a8dc
theus deka <me@theus.be>
2020-02-26 10:45:30 +0100

    Jerome's remarks

--- Service/RestClientService.php
+++ Service/RestClientService.php
@@ -6,20 +6,12 @@ use GuzzleHttp\Client;
 
 class RestClientService
 {
-    
-    /**
-     *
-     * @param string $baseUrl
-     * @return \GuzzleHttp\Client
-     */
-    public function getClient($baseUrl = null, int $timeout = 30)
+    public function getClient(string $baseUrl = null, int $timeout = 30) : Client
     {
         $options = [
-            // You can set any number of default request options.
             'timeout'  => $timeout,
         ];
-        if ($baseUrl) {
-            // Base URI is used with relative requests
+        if ($baseUrl !== null) {
             $options['base_uri'] = $baseUrl;
         }
         return new Client($options);

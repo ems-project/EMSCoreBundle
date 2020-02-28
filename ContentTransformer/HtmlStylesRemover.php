85cc6f79756f2ac44861082634f0b543ba939327
Jérôme Coppée <j.coppee@gmail.com>
2020-02-28 17:55:34 +0100

    refactor(HtmlStylesRemover): re-add hasChanges() method because specific need + add explicit input/output config

--- ContentTransformer/HtmlStylesRemover.php
+++ ContentTransformer/HtmlStylesRemover.php
@@ -30,10 +30,17 @@ class HtmlStylesRemover implements ContentTransformInterface
         $this->doc = $this->initDocument($contentTransformContext->getData());
         $this->xpath = new \DOMXPath($this->doc);
 
-        return $this->removeHtmlStyles();
+        $this->removeHtmlStyles();
+
+        return $this->outputDocument();
     }
 
-    protected function removeHtmlStyles(): ?string
+    public function hasChanges(ContentTransformContext $contentTransformContext): bool
+    {
+        return $this->outputHtmlFormat($contentTransformContext->getData()) !== $contentTransformContext->getTransformedData();
+    }
+
+    protected function removeHtmlStyles(): void
     {
         while ($node = $this->xpath->query("//*[contains(concat(' ', normalize-space(@class), ' '), '{$this->classNamePrefix}')]")->item(0)) {
             $node->parentNode->replaceChild(
@@ -41,10 +48,6 @@ class HtmlStylesRemover implements ContentTransformInterface
                 $node
             );
         }
-
-        $this->removeTemporaryWrapper();
-
-        return rtrim($this->doc->saveHTML());
     }
 
     private function getInnerNode(\DOMNode $node): \DOMNode
@@ -62,16 +65,31 @@ class HtmlStylesRemover implements ContentTransformInterface
 
     protected function initDocument(string $input): \DOMDocument
     {
-        $doc = new \DOMDocument();
+        $doc = new \DOMDocument('1.0', 'UTF-8');
         $doc->preserveWhiteSpace = false;
         $doc->formatOutput = true;
 
-        $doc->loadHtml(
-            $this->addTemporaryWrapper($input),
+        @$doc->loadHtml( // Need @ to bypass the htmlParseEntityRef warnings/errors
+            \mb_convert_encoding($this->addTemporaryWrapper($input), 'HTML-ENTITIES', 'UTF-8'),
             \LIBXML_HTML_NOIMPLIED | \LIBXML_HTML_NODEFDTD
         );
 
         return $doc;
+    }
+
+    protected function outputDocument(): string
+    {
+        $this->removeTemporaryWrapper();
+
+        return $this->outputHtmlFormat($this->doc->saveHTML());
+    }
+
+    private function outputHtmlFormat(string $html): string
+    {
+        $html = \html_entity_decode($html, ENT_QUOTES, 'UTF-8');
+        $html = \rtrim($html);
+
+        return $html;
     }
 
     private function addTemporaryWrapper(string $html): string

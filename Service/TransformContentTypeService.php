fddaa6cdb20ae27ca7270a7e28316d72a4eab2f9
theus77 <me@theus.be>
2020-03-04 11:03:38 +0100

    Better transform command (#301)
    * Avoid to init draft on every document
    * Avoid to interrupt the command onfirst error
    * Don't process draft document

--- Service/TransformContentTypeService.php
+++ Service/TransformContentTypeService.php
@@ -3,13 +3,14 @@
 namespace EMS\CoreBundle\Service;
 
 use Elasticsearch\Client;
-use EMS\CommonBundle\Common\Document;
+use EMS\CommonBundle\Helper\EmsFields;
 use EMS\CoreBundle\ContentTransformer\ContentTransformContext;
 use EMS\CoreBundle\ContentTransformer\ContentTransformInterface;
 use EMS\CoreBundle\Entity\ContentType;
 use EMS\CoreBundle\Entity\DataField;
 use EMS\CoreBundle\Form\DataField\DataFieldType;
 use EMS\CoreBundle\Form\Form\RevisionType;
+use Exception;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 
@@ -54,11 +55,21 @@ class TransformContentTypeService
 
             foreach ($scroll['hits']['hits'] as $hit) {
                 $isChanged = false;
-                $document = new Document($contentType->getName(), $hit['_id'], $hit['_source']);
+                $ouuid = $hit['_id'];
+                $revision = $this->dataService->getNewestRevision($contentType->getName(), $ouuid);
 
-                $revision = $this->dataService->initNewDraft($document->getContentType(), $document->getOuuid(), null, 'TRANSFORM_CONTENT');
+                if ($revision->getDraft()) {
+                    $this->logger->warning('service.data.transform_content_tyoe.cant_process_draft', [
+                        EmsFields::LOG_REVISION_ID_FIELD => $revision->getId(),
+                        EmsFields::LOG_CONTENTTYPE_FIELD => $contentType->getName(),
+                        EmsFields::LOG_ENVIRONMENT_FIELD => $contentType->getEnvironment()->getName(),
+                        EmsFields::LOG_OUUID_FIELD => $revision->getOuuid(),
+                    ]);
+                    yield $revision;
+                    continue;
+                }
+
                 $revisionType = $this->formFactory->create(RevisionType::class, $revision);
-
                 $result = $this->dataService->walkRecursive($revisionType->get('data'), $hit['_source'], function (string $name, $data, DataFieldType $dataFieldType, DataField $dataField) use (&$isChanged) {
                     if ($data === null) {
                         return [];
@@ -82,20 +93,26 @@ class TransformContentTypeService
                 });
 
                 if (!$isChanged) {
-                    $this->dataService->discardDraft($revision, false, 'TRANSFORM_CONTENT');
-                    yield $document;
+                    yield $revision;
                     continue;
                 }
 
-                $rawData = $revision->getRawData();
-                foreach ($result as $key => $value) {
-                    $rawData[$key] = $value;
+
+                try {
+                    $revision = $this->dataService->initNewDraft($contentType->getName(), $ouuid, null, 'TRANSFORM_CONTENT');
+                    $revision->setRawData($result);
+                    $this->dataService->finalizeDraft($revision, $revisionType, 'TRANSFORM_CONTENT');
+                } catch (Exception $e) {
+                    $this->logger->error('service.data.transform_content_tyoe.errer_on_save', [
+                        EmsFields::LOG_REVISION_ID_FIELD => $revision->getId(),
+                        EmsFields::LOG_CONTENTTYPE_FIELD => $contentType->getName(),
+                        EmsFields::LOG_ENVIRONMENT_FIELD => $contentType->getEnvironment()->getName(),
+                        EmsFields::LOG_OUUID_FIELD => $revision->getOuuid(),
+                        EmsFields::LOG_ERROR_MESSAGE_FIELD => $e->getMessage(),
+                        EmsFields::LOG_EXCEPTION_FIELD => $e,
+                    ]);
                 }
-
-                $revision->setRawData($rawData);
-
-                $this->dataService->finalizeDraft($revision, $revisionType, 'TRANSFORM_CONTENT');
-                yield $document;
+                yield $revision;
             }
         }
     }

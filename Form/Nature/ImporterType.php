4ffa5b06df56b5303c24da39f6c8ced9036d6958
theus deka <me@theus.be>
2020-03-11 16:32:55 +0100

    New ImporterType form (receiving a file as archive)

---
+++ Form/Nature/ImporterType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace EMS\CoreBundle\Form\Nature;
+
+use EMS\CoreBundle\EMSCoreBundle;
+use EMS\CoreBundle\Form\Field\FileType;
+use EMS\CoreBundle\Form\Field\SubmitEmsType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImporterType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+           ->add('archive', FileType::class, [
+               'attr' => [
+               ],
+               'meta_fields' => false,
+           ])
+           ->add('import', SubmitEmsType::class, [
+               'attr' => [
+                       'class' => 'btn-danger'
+               ],
+               'icon' => 'glyphicon glyphicon-import'
+           ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'view' => null,
+            'translation_domain' => EMSCoreBundle::TRANS_DOMAIN,
+        ]);
+    }
+}

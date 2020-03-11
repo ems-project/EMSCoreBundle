fd84d7042207422286cd06c65581ba38f04da9c4
theus deka <me@theus.be>
2020-03-11 15:37:59 +0100

    New meta_fields option to be able to reuse the FileType form field without the asset extract mechanism

--- Form/Field/FileType.php
+++ Form/Field/FileType.php
@@ -6,8 +6,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use EMS\CoreBundle\Form\DataField\DateFieldType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FileType extends AbstractType
 {
@@ -38,40 +40,54 @@ class FileType extends AbstractType
                         'class' => 'name'
                 ],
                 'required' => $options['required'],
-        ])
-        ->add('_title', TextType::class, [
-                'attr' => [
-                        'class' => 'title'
-                ],
-                'required' => false,
-        ])
-        ->add('_date', TextType::class, [
-                'attr' => [
-                        'class' => 'date'
-                ],
-                'required' => false,
-        ])
-        ->add('_author', TextType::class, [
-                'attr' => [
-                        'class' => 'author'
-                ],
-                'required' => false,
-        ])
-        ->add('_language', TextType::class, [
-                'attr' => [
-                        'class' => 'language'
-                ],
-                'required' => false,
-        ])
-        ->add('_content', TextareaType::class, [
-                'attr' => [
-                        'class' => 'content',
-                        'rows' => 6,
-                ],
-                'required' => false,
+        ]);
+
+        if ($options['meta_fields']) {
+            $builder->add('_title', TextType::class, [
+                    'attr' => [
+                            'class' => 'title'
+                    ],
+                    'required' => false,
+            ])
+            ->add('_date', TextType::class, [
+                    'attr' => [
+                            'class' => 'date'
+                    ],
+                    'required' => false,
+            ])
+            ->add('_author', TextType::class, [
+                    'attr' => [
+                            'class' => 'author'
+                    ],
+                    'required' => false,
+            ])
+            ->add('_language', TextType::class, [
+                    'attr' => [
+                            'class' => 'language'
+                    ],
+                    'required' => false,
+            ])
+            ->add('_content', TextareaType::class, [
+                    'attr' => [
+                            'class' => 'content',
+                            'rows' => 6,
+                    ],
+                    'required' => false,
+            ]);
+        }
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'meta_fields' => true,
         ]);
     }
 
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+        $view->vars['meta_fields'] = $options['meta_fields'];
+    }
     /**
      *
      * {@inheritdoc}

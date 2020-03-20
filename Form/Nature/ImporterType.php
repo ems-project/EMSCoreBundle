aaa9abf9e4cb89f3c9c22d55972b69660e0aab14
theus deka <me@theus.be>
2020-03-20 15:55:34 +0100

    empty line

--- Form/Nature/ImporterType.php
+++ Form/Nature/ImporterType.php
@@ -13,7 +13,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ImporterType extends AbstractType
 {
-
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder

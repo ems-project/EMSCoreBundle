4355e7acff9539a01ff0a4ec1b39030f8f74f985
David Mattei <david.mattei@smals.be>
2020-04-05 22:47:22 +0200

    hot(): fix indexController using the wrong Client (guzzle)

--- Controller/ContentManagement/IndexController.php
+++ Controller/ContentManagement/IndexController.php
@@ -2,11 +2,11 @@
 
 namespace EMS\CoreBundle\Controller\ContentManagement;
 
+use EMS\CommonBundle\Contracts\Elasticsearch\ClientInterface;
 use EMS\CommonBundle\Twig\RequestRuntime;
 use EMS\CoreBundle\Controller\AppController;
 use EMS\CoreBundle\Service\AliasService;
 use EMS\CoreBundle\Service\IndexService;
-use GuzzleHttp\ClientInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Form\FormRegistryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;

9710ddc3c583af52cc9a18bdf377aede04699f0c
David Meert <david.meert@smals.be>
2020-05-13 12:17:47 +0200

    ref(authtoken): refactor authToken controller

---
+++ Security/Authenticator.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace EMS\CoreBundle\Security;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use EMS\CoreBundle\Entity\AuthToken;
+use EMS\CoreBundle\Entity\User;
+use EMS\CoreBundle\Service\UserService;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class Authenticator
+{
+    /** @var Registry */
+    private $doctrine;
+    /** @var EncoderFactoryInterface */
+    private $encoderFactory;
+    /** @var UserService */
+    private $userService;
+
+    public function __construct(Registry $doctrine, EncoderFactoryInterface $encoderFactory, UserService $userService)
+    {
+        $this->doctrine = $doctrine;
+        $this->encoderFactory = $encoderFactory;
+        $this->userService = $userService;
+    }
+
+    public function authenticate(UsernamePasswordToken $token): void
+    {
+        $user = $this->userService->getUser($token->getUsername());
+        if (empty($user)) {
+            throw new \RuntimeException("User not found");
+        }
+
+        $encoder = $this->encoderFactory->getEncoder($user);
+        if ($encoder->isPasswordValid($user->getPassword(), $token->getCredentials(), $user->getSalt())) {
+            $token->eraseCredentials();
+            $token->setUser($user);
+        }
+    }
+
+    public function generateAuthToken(UsernamePasswordToken $token): AuthToken
+    {
+        $authToken = new AuthToken($this->getUser($token));
+
+        $em = $this->doctrine->getManager();
+        $em->persist($authToken);
+        $em->flush();
+
+        return $authToken;
+    }
+
+    public function failedResponse(): Response
+    {
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->setContent(json_encode([
+            'success' => false,
+            'acknowledged' => true,
+            'error' => ['Unauthorized Error'],
+        ]))->setStatusCode(401);
+        return $response;
+    }
+
+    public function successResponse(string $content): Response
+    {
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->setContent($content)->setStatusCode(401);
+        return $response;
+    }
+
+    private function getUser(UsernamePasswordToken $token): User
+    {
+        if (!$token->getUser() instanceof UserInterface) {
+            $this->authenticate($token);
+        }
+
+        $user = $token->getUser();
+        if (!$user instanceof User) {
+            throw new \RuntimeException(sprintf('User should be of type %s', User::class));
+        }
+        return $user;
+    }
+}

187f4c741c5caa043116a274f5a36da9f005e3ec
David Mattei <david.mattei@smals.be>
2020-07-14 13:15:34 +0200

    update pgsql migration

--- Resources/DoctrineMigrations/pdo_pgsql/Version20200714111326.php
+++ Resources/DoctrineMigrations/pdo_pgsql/Version20200714111326.php
@@ -8,15 +8,13 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200714082948 extends AbstractMigration
+final class Version20200714111326 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('DROP SEQUENCE form_submission_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE form_submission_file_id_seq CASCADE');
         $this->addSql('CREATE TABLE form_submission (id UUID NOT NULL, created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, modified TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, name VARCHAR(255) NOT NULL, instance VARCHAR(255) NOT NULL, locale VARCHAR(2) NOT NULL, data JSON NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN form_submission.id IS \'(DC2Type:uuid)\'');
         $this->addSql('CREATE TABLE form_submission_file (id UUID NOT NULL, form_submission_id UUID DEFAULT NULL, created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, modified TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, file BYTEA NOT NULL, filename VARCHAR(255) NOT NULL, form_field VARCHAR(255) NOT NULL, mime_type VARCHAR(1024) NOT NULL, size BIGINT NOT NULL, PRIMARY KEY(id))');
@@ -32,8 +30,6 @@ final class Version20200714082948 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('ALTER TABLE form_submission_file DROP CONSTRAINT FK_AEFF00A6422B0E0C');
-        $this->addSql('CREATE SEQUENCE form_submission_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE form_submission_file_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('DROP TABLE form_submission');
         $this->addSql('DROP TABLE form_submission_file');
     }

09ab5a12b8cdca3363dc0110cdaf14a78166c301
Laurens Mertens <laurensmertens@yahoo.com>
2020-10-22 15:40:13 +0200

    PHPStan Fixes

--- Validator/Constraints/IsDifferentPasswordValidator.php
+++ Validator/Constraints/IsDifferentPasswordValidator.php
@@ -7,13 +7,15 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 class IsDifferentPasswordValidator extends ConstraintValidator
 {
-    public function validate($value, Constraint $constraint)
+    const ERROR_MESSAGE = 'Password has to be different from the previous one';
+
+    public function validate($value, Constraint $constraint): bool
     {
         $currentPassword = $_POST['fos_user_change_password_form']['current_password'];
         $newPassword = $_POST['fos_user_change_password_form']['plainPassword']['first'];
 
         if ($currentPassword === $newPassword) {
-            $this->context->addViolation($constraint->message);
+            $this->context->addViolation(self::ERROR_MESSAGE);
             return false;
         }
         return true;

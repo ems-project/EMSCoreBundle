9dc4c1d1770698731f26b830168d31a20c0aeb87
Laurens Mertens <laurensmertens@yahoo.com>
2020-10-22 15:40:13 +0200

    PHPStan Fixes

--- Validator/Constraints/IsDifferentPassword.php
+++ Validator/Constraints/IsDifferentPassword.php
@@ -9,9 +9,10 @@ use Symfony\Component\Validator\Constraint;
  */
 class IsDifferentPassword extends Constraint
 {
+    /** @var string */
     public $message = 'Password has to be different from the previous one';
 
-    public function getTargets()
+    public function getTargets(): string
     {
         return Constraint::CLASS_CONSTRAINT;
     }

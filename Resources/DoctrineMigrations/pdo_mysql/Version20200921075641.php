9d46ca5b7e9f3d78dc01ca73608cffd5d7a550e1
Mathieu De Keyzer <me@theus.be>
2020-11-01 12:08:07 +0100

    fix(doctrine): not existing expire_date

--- Resources/DoctrineMigrations/pdo_mysql/Version20200921075641.php
+++ Resources/DoctrineMigrations/pdo_mysql/Version20200921075641.php
@@ -13,14 +13,12 @@ final class Version20200921075641 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $this->addSql('ALTER TABLE form_submission DROP COLUMN expire_date ;');
         $this->addSql('ALTER TABLE form_submission ADD expire_date DATE DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $this->addSql('ALTER TABLE form_submission DROP COLUMN expire_date ;');
-        $this->addSql('ALTER TABLE form_submission ADD expire_date VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE form_submission DROP COLUMN expire_date');
     }
 }

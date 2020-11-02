6cee6c86b40fdb43727e4818879cd5c0b405bfce
Laurens Mertens <laurensmertens@yahoo.com>
2020-11-02 12:31:19 +0100

    Add flash message password change required

--- EventSubscriber/ChangePasswordSubscriber.php
+++ EventSubscriber/ChangePasswordSubscriber.php
@@ -4,6 +4,7 @@ namespace EMS\CoreBundle\EventSubscriber;
 
 use EMS\CoreBundle\Service\UserService;
 use FOS\UserBundle\Event\FilterUserResponseEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
 use FOS\UserBundle\FOSUserEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
@@ -23,14 +24,22 @@ class ChangePasswordSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            FOSUserEvents::CHANGE_PASSWORD_COMPLETED => 'updateForcePasswordChange'
+            FOSUserEvents::CHANGE_PASSWORD_COMPLETED => 'updateForcePasswordChange',
+            FOSUserEvents::CHANGE_PASSWORD_INITIALIZE => 'showMustChangePasswordMessage'
         );
     }
 
     public function updateForcePasswordChange(FilterUserResponseEvent $event)
     {
-        $user = $event->getUser();
-        $user->setForcePasswordChange(false);
+        $user = $event->getUser()->setForcePasswordChange(false);
+        $event->getRequest()->getSession()->getFlashBag()->clear();
         $this->userService->updateUser($user);
+    }
+
+    public function showMustChangePasswordMessage(GetResponseUserEvent $event)
+    {
+        if ($event->getUser()->getForcePasswordChange()) {
+            $event->getRequest()->getSession()->getFlashBag()->add('notice', 'You are required to change your password.');
+        }
     }
 }

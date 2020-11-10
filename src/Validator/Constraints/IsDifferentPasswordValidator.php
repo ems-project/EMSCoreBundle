21c78219f0efb8d4408570059071f5cb1fb0917c
Laurens Mertens <laurensmertens@yahoo.com>
2020-11-10 16:14:59 +0100

    No DTO just try catch

--- src/Validator/Constraints/IsDifferentPasswordValidator.php
+++ src/Validator/Constraints/IsDifferentPasswordValidator.php
@@ -4,33 +4,30 @@ namespace EMS\CoreBundle\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
-use EMS\CoreBundle\Validator\DTO\Credentials;
 
 class IsDifferentPasswordValidator extends ConstraintValidator
 {
     /**
-     * @param mixed                          $value
+     * @param mixed $value
      * @param Constraint|IsDifferentPassword $constraint
      */
     public function validate($value, $constraint): bool
     {
-        if (! $constraint instanceof IsDifferentPassword) {
+        if (!$constraint instanceof IsDifferentPassword) {
             return false;
         }
 
         try {
-            $credentials = new Credentials(
-                $_POST['fos_user_change_password_form']['current_password'],
-                $_POST['fos_user_change_password_form']['plainPassword']['first']
-            );
+            $old = $_POST['fos_user_change_password_form']['current_password'];
+            $new = $_POST['fos_user_change_password_form']['plainPassword']['first'];
+            if ($old === $new) {
+                $this->context->addViolation($constraint->message);
+                return false;
+            }
         } catch (\Exception $e) {
-            return true;
+
         }
 
-        if ($credentials->getCurrentPassword() === $credentials->getNewPassword()) {
-            $this->context->addViolation($constraint->message);
-            return false;
-        }
         return true;
     }
 }

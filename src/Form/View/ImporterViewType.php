9fff7c9f5aafc21774575219da96ab5ce5fb567f
Mathieu De Keyzer <me@theus.be>
2020-11-30 01:17:58 +0100

    refactor: ImporterViewType and DataLinkFieldType

--- src/Form/View/ImporterViewType.php
+++ src/Form/View/ImporterViewType.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace EMS\CoreBundle\Form\View;
 
-use Elasticsearch\Client;
 use EMS\CommonBundle\Helper\EmsFields;
 use EMS\CoreBundle\Entity\UserInterface;
 use EMS\CoreBundle\Entity\View;
@@ -33,17 +32,14 @@ class ImporterViewType extends ViewType
     private $security;
     /** @var Router */
     private $router;
-    /** @var Client */
-    private $client;
 
-    public function __construct(FormFactory $formFactory, Environment $twig, Client $client, LoggerInterface $logger, FileService $fileService, JobService $jobService, TokenStorageInterface $security, Router $router)
+    public function __construct(FormFactory $formFactory, Environment $twig, LoggerInterface $logger, FileService $fileService, JobService $jobService, TokenStorageInterface $security, Router $router)
     {
         parent::__construct($formFactory, $twig, $logger);
         $this->fileService = $fileService;
         $this->jobService = $jobService;
         $this->security = $security;
         $this->router = $router;
-        $this->client = $client;
     }
 
     public function getLabel(): string

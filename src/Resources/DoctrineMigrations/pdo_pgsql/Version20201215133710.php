bdd3251d1f293593676249281fd0673fe1cd50d6
Mathieu De Keyzer <me@theus.be>
2020-12-15 14:41:06 +0100

    feat: channel entity pgsql migrate script

---
+++ src/Resources/DoctrineMigrations/pdo_pgsql/Version20201215133710.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20201215133710 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE TABLE channel (id UUID NOT NULL, created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, modified TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, name VARCHAR(255) NOT NULL, public BOOLEAN DEFAULT \'false\' NOT NULL, slug VARCHAR(255) NOT NULL, options JSON DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN channel.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('ALTER TABLE job DROP arguments');
+        $this->addSql('ALTER TABLE job DROP service');
+        $this->addSql('ALTER TABLE form_submission ALTER label SET NOT NULL');
+        $this->addSql('ALTER TABLE form_submission ALTER deadline_date SET NOT NULL');
+        $this->addSql('ALTER TABLE form_submission ALTER expire_date SET NOT NULL');
+        $this->addSql('DROP INDEX asset_key_index');
+        $this->addSql('ALTER TABLE asset_storage DROP context');
+        $this->addSql('ALTER TABLE asset_storage DROP last_update_date');
+        $this->addSql('ALTER TABLE asset_storage ALTER hash TYPE VARCHAR(1024)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_37945A62D1B862B8 ON asset_storage (hash)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP TABLE channel');
+        $this->addSql('DROP INDEX UNIQ_37945A62D1B862B8');
+        $this->addSql('ALTER TABLE asset_storage ADD context VARCHAR(128) DEFAULT NULL');
+        $this->addSql('ALTER TABLE asset_storage ADD last_update_date INT NOT NULL');
+        $this->addSql('ALTER TABLE asset_storage ALTER hash TYPE VARCHAR(128)');
+        $this->addSql('CREATE UNIQUE INDEX asset_key_index ON asset_storage (hash, context)');
+        $this->addSql('ALTER TABLE form_submission ALTER expire_date DROP NOT NULL');
+        $this->addSql('ALTER TABLE form_submission ALTER label DROP NOT NULL');
+        $this->addSql('ALTER TABLE form_submission ALTER deadline_date DROP NOT NULL');
+        $this->addSql('ALTER TABLE job ADD arguments JSON DEFAULT NULL');
+        $this->addSql('ALTER TABLE job ADD service VARCHAR(255) DEFAULT NULL');
+        $this->addSql('COMMENT ON COLUMN job.arguments IS \'(DC2Type:json_array)\'');
+    }
+}

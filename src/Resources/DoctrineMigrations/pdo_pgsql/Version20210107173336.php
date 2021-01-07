71681a2261c4193e6473c6f2fae8c652f16bf18e
David Mattei <david.mattei@smals.be>
2021-01-07 20:09:12 +0100

    fix: doc migrations remove singleType index

--- src/Resources/DoctrineMigrations/pdo_pgsql/Version20210107173336.php
+++ src/Resources/DoctrineMigrations/pdo_pgsql/Version20210107173336.php
@@ -17,6 +17,9 @@ final class Version20210107173336 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
         $this->addSql('ALTER TABLE environment ADD update_referrers BOOLEAN DEFAULT \'false\' NOT NULL');
+
+        $this->addSql('DROP SEQUENCE single_type_index_id_seq CASCADE');
+        $this->addSql('DROP TABLE single_type_index');
     }
 
     public function down(Schema $schema): void
@@ -24,5 +27,12 @@ final class Version20210107173336 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
         $this->addSql('ALTER TABLE environment DROP update_referrers');
+
+        $this->addSql('CREATE SEQUENCE single_type_index_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE single_type_index (id BIGINT NOT NULL, content_type_id BIGINT DEFAULT NULL, environment_id INT DEFAULT NULL, created TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, modified TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_FEAD46B31A445520 ON single_type_index (content_type_id)');
+        $this->addSql('CREATE INDEX IDX_FEAD46B3903E3A94 ON single_type_index (environment_id)');
+        $this->addSql('ALTER TABLE single_type_index ADD CONSTRAINT fk_fead46b31a445520 FOREIGN KEY (content_type_id) REFERENCES content_type (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE single_type_index ADD CONSTRAINT fk_fead46b3903e3a94 FOREIGN KEY (environment_id) REFERENCES environment (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 }

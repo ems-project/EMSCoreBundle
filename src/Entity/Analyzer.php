47ce3289ff3c82e9a1c48e9c6834988a29473e1a
Mathieu De Keyzer <me@theus.be>
2021-01-18 13:56:27 +0100

    feat: analyzers export

--- src/Entity/Analyzer.php
+++ src/Entity/Analyzer.php
@@ -279,17 +279,7 @@ class Analyzer extends JsonDeserializer implements \JsonSerializable
         return $this->orderKey;
     }
 
-    /**
-     * Specify data which should be serialized to JSON.
-     *
-     * @see https://php.net/manual/en/jsonserializable.jsonserialize.php
-     *
-     * @return mixed data which can be serialized by <b>json_encode</b>,
-     *               which is a value of any type other than a resource
-     *
-     * @since 5.4.0
-     */
-    public function jsonSerialize()
+    public function jsonSerialize(): JsonClass
     {
         $json = new JsonClass(\get_object_vars($this), __CLASS__);
         $json->removeProperty('id');

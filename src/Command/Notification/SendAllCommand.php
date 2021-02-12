c33ff69eba44ead40431e1bcfbefa97550f842cb
David Mattei <david.mattei@smals.be>
2021-02-12 11:05:37 +0100

    chore: rename SendNotifications to SendAll

--- src/Command/Notification/SendAllCommand.php
+++ src/Command/Notification/SendAllCommand.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-final class SendNotificationsCommand extends Command
+final class SendAllCommand extends Command
 {
     private Registry $doctrine;
     private NotificationService $notificationService;

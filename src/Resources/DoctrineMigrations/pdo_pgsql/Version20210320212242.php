76b0800d02c4852cc1c1948fc265dc1aeea0ab36
Mathieu De Keyzer <me@theus.be>
2021-03-20 22:27:21 +0100

    fix: ensure that there is no started but never done job

---
+++ src/Resources/DoctrineMigrations/pdo_pgsql/Version20210320212242.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20210320212242 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('UPDATE job SET done = TRUE WHERE done IS FALSE AND started is TRUE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+    }
+}

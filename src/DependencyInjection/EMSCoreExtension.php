ae790af87349cf1e5fa0c4b7a8e94e5799f533ca
Mathieu De Keyzer <me@theus.be>
2021-03-20 11:17:57 +0100

    feat: add default_bulk_size bundle configuration to bundle container extension

--- src/DependencyInjection/EMSCoreExtension.php
+++ src/DependencyInjection/EMSCoreExtension.php
@@ -68,6 +68,7 @@ class EMSCoreExtension extends Extension implements PrependExtensionInterface
         $container->setParameter('ems_core.tika_download_url', $config['tika_download_url']);
         $container->setParameter('ems_core.log_by_pass', $config['log_by_pass']);
         $container->setParameter('ems_core.log_level', $config['log_level']);
+        $container->setParameter('ems_core.default_bulk_size', $config['default_bulk_size']);
 
         $this->loadLdap($container, $yamlLoader, $config['ldap'] ?? []);
     }

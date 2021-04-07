196c7ede54301db44853a9b3696a9597ecc188a1
AdrienHq <34314047+AdrienHq@users.noreply.github.com>
2021-04-07 11:44:57 +0200

    feat: QuerySearch Service

---
+++ src/Service/QuerySearchService.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Service;
+
+use EMS\CommonBundle\Helper\Text\Encoder;
+use EMS\CoreBundle\Entity\QuerySearch;
+use EMS\CoreBundle\Repository\QuerySearchRepository;
+use Psr\Log\LoggerInterface;
+
+final class QuerySearchService implements EntityServiceInterface
+{
+    private QuerySearchRepository $querySearchRepository;
+    private LoggerInterface $logger;
+
+    public function __construct(QuerySearchRepository $querySearchRepository, LoggerInterface $logger)
+    {
+        $this->querySearchRepository = $querySearchRepository;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @return QuerySearch[]
+     */
+    public function getAll(): array
+    {
+        return $this->querySearchRepository->getAll();
+    }
+
+    public function update(QuerySearch $querySearch): void
+    {
+        if (0 === $querySearch->getOrderKey()) {
+            $querySearch->setOrderKey($this->querySearchRepository->counter() + 1);
+        }
+        $encoder = new Encoder();
+        $label = $querySearch->getLabel();
+        if (null === $label) {
+            throw new \RuntimeException('Unexpected null label');
+        }
+        $webalized = $encoder->webalize($label);
+        if (null === $webalized) {
+            throw new \RuntimeException('Unexpected null webalized label');
+        }
+        $querySearch->setLabel($webalized);
+        $this->querySearchRepository->create($querySearch);
+    }
+
+    public function delete(QuerySearch $querySearch): void
+    {
+        $label = $querySearch->getLabel();
+        $this->querySearchRepository->delete($querySearch);
+        $this->logger->warning('log.service.querySearch.delete', [
+            'label' => $label,
+        ]);
+    }
+
+    /**
+     * @param string[] $ids
+     */
+    public function deleteByIds(array $ids): void
+    {
+        foreach ($this->querySearchRepository->getByIds($ids) as $querySearch) {
+            $this->delete($querySearch);
+        }
+    }
+
+    /**
+     * @param array<string, int> $ids
+     */
+    public function reorderByIds(array $ids): void
+    {
+        foreach ($this->querySearchRepository->getByIds(\array_keys($ids)) as $querySearch) {
+            $querySearch->setOrderKey($ids[$querySearch->getId()] ?? 0);
+            $this->querySearchRepository->create($querySearch);
+        }
+    }
+
+    public function isSortable(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @param mixed $context
+     *
+     * @return QuerySearch[]
+     */
+    public function get(int $from, int $size, $context = null): array
+    {
+        if (null !== $context) {
+            throw new \RuntimeException('Unexpected context');
+        }
+
+        return $this->querySearchRepository->get($from, $size);
+    }
+
+    public function getEntityName(): string
+    {
+        return 'querySearch';
+    }
+
+    /**
+     * @param mixed $context
+     */
+    public function count($context = null): int
+    {
+        if (null !== $context) {
+            throw new \RuntimeException('Unexpected non-null object');
+        }
+
+        return $this->querySearchRepository->counter();
+    }
+}

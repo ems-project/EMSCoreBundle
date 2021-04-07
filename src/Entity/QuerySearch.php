0d8d2841f58b59b7e352b3f911177f08e86e4b5a
AdrienHq <34314047+AdrienHq@users.noreply.github.com>
2021-04-07 10:37:35 +0200

    feat: QuerySearch Entity & Repo

---
+++ src/Entity/QuerySearch.php
@@ -0,0 +1,174 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * @ORM\Table(name="query_search")
+ * @ORM\Entity()
+ * @ORM\HasLifecycleCallbacks()
+ */
+class QuerySearch implements EntityInterface
+{
+    /**
+     * @var UuidInterface
+     *
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class=UuidGenerator::class)
+     */
+    private $id;
+
+    /**
+     * @var \Datetime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var \Datetime
+     *
+     * @ORM\Column(name="modified", type="datetime")
+     */
+    private $modified;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="label", type="string", length=255)
+     */
+    private $label;
+
+    /**
+     * @var Environment[]
+     * @ORM\ManyToMany(targetEntity="Environment", inversedBy="contentTypesHavingThisAsDefault")
+     * @ORM\JoinColumn(name="environment_id", referencedColumnName="id")
+     */
+    protected $environments;
+
+    /**
+     * @var string
+     * 
+     * @ORM\Column(name="query", type="json", nullable=true)
+     */
+    private $query;
+
+    /**
+     * @var array<string, mixed>
+     *
+     * @ORM\Column(name="options", type="json", nullable=true)
+     */
+    private $options;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="order_key", type="integer")
+     */
+    private $orderKey;
+
+    public function __construct()
+    {
+        $now = new \DateTime();
+        $this->id = Uuid::uuid4();
+        $this->created = $now;
+        $this->modified = $now;
+        $this->environments = [];
+        $this->query = '{}';
+        $this->options = [
+            'translationContentType' => 'label',
+            'routeContentType' => 'route',
+            'templateContentType' => 'template',
+            'searchConfig' => '{}',
+        ];
+    }
+
+    public function getId(): string
+    {
+        return $this->id->toString();
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function updateModified(): void
+    {
+        $this->modified = new \DateTime();
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): void
+    {
+        $this->label = $label;
+    }
+
+    /**
+     * @return Environment[]
+     */
+    public function getEnvironments(): array
+    {
+        return $this->environments;
+    }
+
+    /**
+     * @param Environment[] $environments
+     */
+    public function setEnvironments(array $environments): void
+    {
+        $this->environments = $environments;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQuery(): string
+    {
+        return $this->query;
+    }
+    
+    /**
+     * @param string $query
+     */
+    public function setQuery(string $query): void
+    {
+        $this->query = $query;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function getOptions(): array
+    {
+        return $this->options ?? [];
+    }
+
+    /**
+     * @param array<string, mixed> $options
+     */
+    public function setOptions(array $options): void
+    {
+        $this->options = $options;
+    }
+
+    public function getOrderKey(): int
+    {
+        return $this->orderKey ?? 0;
+    }
+
+    public function setOrderKey(int $orderKey): void
+    {
+        $this->orderKey = $orderKey;
+    }
+}
---
+++ src/Repository/QuerySearchRepository.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use EMS\CoreBundle\Entity\QuerySearch;
+
+final class QuerySearchRepository extends ServiceEntityRepository
+{
+    public function __construct(Registry $registry)
+    {
+        parent::__construct($registry, QuerySearch::class);
+    }
+
+    /**
+     * @return QuerySearch[]
+     */
+    public function getAll(): array
+    {
+        return $this->findBy([], ['orderKey' => 'ASC']);
+    }
+
+    public function counter(): int
+    {
+        return parent::count([]);
+    }
+
+    public function create(QuerySearch $querySearch): void
+    {
+        $this->getEntityManager()->persist($querySearch);
+        $this->getEntityManager()->flush();
+    }
+
+    public function delete(QuerySearch $querySearch): void
+    {
+        $this->getEntityManager()->remove($querySearch);
+        $this->getEntityManager()->flush();
+    }
+
+    /**
+     * @param string[] $ids
+     *
+     * @return QuerySearch[]
+     */
+    public function getByIds(array $ids): array
+    {
+        $queryBuilder = $this->createQueryBuilder('querysearch');
+        $queryBuilder->where('querysearch.id IN (:ids)')
+            ->setParameter('ids', $ids);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * @return QuerySearch[]
+     */
+    public function get(int $from, int $size): array
+    {
+        $query = $this->createQueryBuilder('c')
+            ->orderBy('c.orderKey', 'ASC')
+            ->setFirstResult($from)
+            ->setMaxResults($size)
+            ->getQuery();
+
+        return $query->execute();
+    }
+}

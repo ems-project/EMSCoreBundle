3a6ffeae9bdb953e944f143dafa76a63b9bd5c05
Mathieu De Keyzer <me@theus.be>
2021-04-08 16:51:35 +0200

    fix: roles column is not orderable

--- src/Form/Data/RolesTableColumn.php
+++ src/Form/Data/RolesTableColumn.php
@@ -15,4 +15,9 @@ final class RolesTableColumn extends TableColumn
     {
         return 'emsco_form_table_column_data_value_roles';
     }
+
+    public function getOrderable(): bool
+    {
+        return false;
+    }
 }

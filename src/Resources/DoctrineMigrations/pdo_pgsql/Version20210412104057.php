b9939722dffdfac4673b42cc9d1096a35f5e8299
Mathieu De Keyzer <me@theus.be>
2021-04-12 13:07:44 +0200

    chore: phpcs

--- src/Resources/DoctrineMigrations/pdo_pgsql/Version20210412104057.php
+++ src/Resources/DoctrineMigrations/pdo_pgsql/Version20210412104057.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Application\Migrations;
 
@@ -10,18 +12,18 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20210412104057 extends AbstractMigration
 {
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('ALTER TABLE wysiwyg_styles_set ADD table_default_css VARCHAR(255) DEFAULT \'table table-bordered\' NOT NULL');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('ALTER TABLE wysiwyg_styles_set DROP table_default_css');
     }

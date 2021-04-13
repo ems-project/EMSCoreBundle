03cf05603b33b4ce1fc5a21fa17cca821056d808
Mathieu De Keyzer <me@theus.be>
2021-04-13 12:56:51 +0200

    chore: phpcs

--- src/Resources/DoctrineMigrations/pdo_mysql/Version20210412144211.php
+++ src/Resources/DoctrineMigrations/pdo_mysql/Version20210412144211.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Application\Migrations;
 
@@ -10,18 +12,18 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20210412144211 extends AbstractMigration
 {
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE uploaded_asset ADD hidden TINYINT(1) DEFAULT \'0\' NOT NULL, ADD head_last DATETIME DEFAULT NULL, ADD head_in LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\'');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE uploaded_asset DROP hidden, DROP head_last, DROP head_in');
     }

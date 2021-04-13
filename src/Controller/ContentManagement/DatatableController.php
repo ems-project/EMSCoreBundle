bbae450a6c5764df50a630b7e743d5b89da361dd
Mathieu De Keyzer <me@theus.be>
2021-04-14 00:09:30 +0200

    feat: datatable controller and ems_core_datatable_ajax_elastica route

---
+++ src/Controller/ContentManagement/DatatableController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Controller\ContentManagement;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class DatatableController
+{
+    public function ajaxElastica(Request $request, string $hashConfig): Response
+    {
+    }
+}

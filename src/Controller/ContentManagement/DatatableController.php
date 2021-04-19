f62daa6c85d01d3caea7df4410576dabfbfebb97
Mathieu De Keyzer <me@theus.be>
2021-04-19 20:05:28 +0200

    fix: new ems_core_datatable_excel_elastica route generating an excel fron a datatable config

--- src/Controller/ContentManagement/DatatableController.php
+++ src/Controller/ContentManagement/DatatableController.php
@@ -4,20 +4,26 @@ declare(strict_types=1);
 
 namespace EMS\CoreBundle\Controller\ContentManagement;
 
+use EMS\CommonBundle\Contracts\SpreadsheetGeneratorServiceInterface;
 use EMS\CoreBundle\Helper\DataTableRequest;
 use EMS\CoreBundle\Service\DatatableService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 final class DatatableController extends AbstractController
 {
     private DatatableService $datatableService;
+    private Environment $twig;
+    private SpreadsheetGeneratorServiceInterface $spreadsheetGeneratorService;
 
-    public function __construct(DatatableService $datatableService)
+    public function __construct(DatatableService $datatableService, Environment $twig, SpreadsheetGeneratorServiceInterface $spreadsheetGeneratorService)
     {
         $this->datatableService = $datatableService;
+        $this->twig = $twig;
+        $this->spreadsheetGeneratorService = $spreadsheetGeneratorService;
     }
 
     public function ajaxElastica(Request $request, string $hashConfig): Response
@@ -30,5 +36,36 @@ final class DatatableController extends AbstractController
             'dataTableRequest' => $dataTableRequest,
             'table' => $table,
         ], new JsonResponse());
+    }
+
+    public function excelElastica(string $hashConfig): Response
+    {
+        $table = $this->datatableService->generateDatatableFromHash($hashConfig);
+        $headers = [];
+        foreach ($table->getColumns() as $column) {
+            $headers[] = $column->getTitleKey();
+        }
+        $rows = [$headers];
+
+        while ($table->next()) {
+            foreach ($table as $line) {
+                $row = [];
+                foreach ($table->getColumns() as $column) {
+                    $row[] = $this->twig->render('@EMSCore/datatable/excel-cell.html.twig', [
+                        'line' => $line,
+                        'column' => $column,
+                    ]);
+                }
+                $rows[] = $row;
+            }
+        }
+
+        $spreadsheetConfig = [
+            'sheets' => [[
+                'name' => 'sheet',
+                'rows' => $rows,
+        ]], ];
+
+        return $this->spreadsheetGeneratorService->generateSpreadsheet($spreadsheetConfig);
     }
 }

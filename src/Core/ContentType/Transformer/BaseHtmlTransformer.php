b99997f65906e0ccb6ebffef5a9833fe09670696
David mattei <d.mattei89@gmail.com>
2021-07-09 16:20:41 +0200

    feat: content transformer remove

---
+++ src/Core/ContentType/Transformer/BaseHtmlTransformer.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Core\ContentType\Transformer;
+
+use Symfony\Component\DomCrawler\Crawler;
+
+abstract class BaseHtmlTransformer extends AbstractTransformer
+{
+    protected function setTransformed(TransformContext $context, Crawler $crawler): void
+    {
+        $data = $context->getData();
+        $transformed = $crawler->outerHtml();
+
+        if (false === \strpos($data, '<html>')) {
+            $transformed = \str_replace(['<html>', '</html>'], '', $transformed);
+        }
+        if (false === \strpos($data, '<body>')) {
+            $transformed = \str_replace(['<body>', '</body>'], '', $transformed);
+        }
+
+        $context->setTransformed($transformed);
+    }
+
+    /**
+     * @return \Generator|\DOMElement[]
+     */
+    protected function crawl(Crawler $crawler, string $xPath): \Generator
+    {
+        foreach ($crawler->filterXPath($xPath) as $element) {
+            if ($element instanceof \DOMElement) {
+                yield $element;
+            }
+        }
+    }
+}

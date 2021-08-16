2c0b10fd6c5e371a6e2308bbce591d2e6d40514f
Mathieu De Keyzer <me@theus.be>
2021-08-16 18:27:49 +0200

    feat: second TDD

--- tests/Unit/Core/Service/XliffTest.php
+++ tests/Unit/Core/Service/XliffTest.php
@@ -28,6 +28,12 @@ class XliffTest extends KernelTestCase
   <p>All rights reserved (c) Gandalf Inc.</p>
  </body>
 </html>',
+        '<p>Hello</p>',
+    ];
+
+    private array $htmlTarget = [
+        '',
+        '<p>Bonjour</p>',
     ];
 
     /**
@@ -35,6 +41,7 @@ class XliffTest extends KernelTestCase
      */
     private array $htmlResults = [
         '<body><group restype="x-html-html"><group restype="x-html-body"><trans-unit id="1" restype="x-html-h1" html:class="title"><source xml:lang="en">Report</source></trans-unit><group restype="table" html:border="1" html:width="100%"><group restype="row"><trans-unit id="2" restype="cell" html:valign="top"><source xml:lang="en">Text in cell r1-c1</source></trans-unit><trans-unit id="3" restype="cell" html:valign="top"><source xml:lang="en">Text in cell r1-c2</source></trans-unit></group><group restype="row"><trans-unit id="4" restype="cell" html:bgcolor="#C0C0C0"><source xml:lang="en">Text in cell r2-c1</source></trans-unit><trans-unit id="5" restype="cell"><source xml:lang="en">Text in cell r2-c2</source></trans-unit></group></group><trans-unit id="6" restype="x-html-p"><source xml:lang="en">All rights reserved (c) Gandalf Inc.</source></trans-unit></group></group></body>',
+        '<body><group restype="x-html-html"><group restype="x-html-body"><trans-unit id="7" restype="x-html-p"><source xml:lang="en">Hello</source><target xml:lang="fr">Bonjour</target></trans-unit></group></group></body>',
     ];
 
     private XliffService $xliffService;
@@ -46,12 +53,14 @@ class XliffTest extends KernelTestCase
 
     public function testXliffExport(): void
     {
+        $loop = 0;
         foreach ($this->htmlSources as $htmlSource) {
             $node = new \SimpleXMLElement('<body/>');
-            $this->xliffService->htmlNode($node, $htmlSource, '', 'en', 'fr');
+            $this->xliffService->htmlNode($node, $htmlSource, $this->htmlTarget[$loop], 'en', 'fr');
             $xml = \explode("\n", $node->saveXML());
             $this->assertArrayHasKey(1, $xml);
-            $this->assertEquals($xml[1], $this->htmlResults[0]);
+            $this->assertEquals($xml[1], $this->htmlResults[$loop]);
+            ++$loop;
         }
     }
 }

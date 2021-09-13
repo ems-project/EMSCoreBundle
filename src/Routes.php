bdca92a354a01e09593ec711fe5418810279ba6c
Mathieu De Keyzer <me@theus.be>
2021-09-14 01:42:46 +0200

    feat: new routes

--- src/Routes.php
+++ src/Routes.php
@@ -5,4 +5,8 @@ namespace EMS\CoreBundle;
 class Routes
 {
     public const EditRevision = 'emsco_edit_revision';
+    public const ViewRevision = 'emsco_view_revisions';
+    public const DiscardDraft = 'emsco_discard_draft';
+    public const DraftInProgress = 'emsco_draft_in_progress';
+    public const DraftInProgressAjax = 'emsco_draft_in_progress_ajax';
 }

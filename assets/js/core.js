c4a3ad6f95de3c647365ee21bfb13db4d62c8124
Mathieu De Keyzer <me@theus.be>
2021-11-06 12:56:26 +0100

    fix: error when no CKeditor in the page

--- assets/js/core.js
+++ assets/js/core.js
@@ -68,16 +68,18 @@ require('jquery-match-height');
 
 //Fix issue CK editor in bootstrap model
 //https://ckeditor.com/old/forums/Support/Issue-with-Twitter-Bootstrap#comment-127719
-$.fn.modal.Constructor.prototype.enforceFocus = function() {
-    const modal_this = this
-    $(document).on('focusin.modal', function (e) {
-        if (modal_this.$element[0] !== e.target && !modal_this.$element.has(e.target).length
-            && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select')
-            && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_text')) {
-            modal_this.$element.focus()
-        }
-    })
-};
+if ($.fn.modal !== undefined) {
+    $.fn.modal.Constructor.prototype.enforceFocus = function() {
+        const modal_this = this
+        $(document).on('focusin.modal', function (e) {
+            if (modal_this.$element[0] !== e.target && !modal_this.$element.has(e.target).length
+                && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select')
+                && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_text')) {
+                modal_this.$element.focus()
+            }
+        })
+    };
+}
 
 require('./helpers');
 require('./initEms');

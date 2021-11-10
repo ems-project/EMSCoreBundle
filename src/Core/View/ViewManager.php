16c111593b3d7d10218637b1e3c511c930987adb
Mathieu De Keyzer <me@theus.be>
2021-11-10 07:39:19 +0100

    feat: view manager

---
+++ src/Core/View/ViewManager.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Core\View;
+
+use EMS\CommonBundle\Helper\Text\Encoder;
+use EMS\CoreBundle\Entity\View;
+use EMS\CoreBundle\Repository\ViewRepository;
+use EMS\CoreBundle\Service\EntityServiceInterface;
+use Psr\Log\LoggerInterface;
+
+class ViewManager implements EntityServiceInterface
+{
+    private ViewRepository $viewRepository;
+    private LoggerInterface $logger;
+
+    public function __construct(ViewRepository $viewRepository, LoggerInterface $logger)
+    {
+        $this->viewRepository = $viewRepository;
+        $this->logger = $logger;
+    }
+
+    public function isSortable(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return View[]
+     */
+    public function get(int $from, int $size, ?string $orderField, string $orderDirection, string $searchValue, $context = null): array
+    {
+        if (null !== $context) {
+            throw new \RuntimeException('Unexpected not null context');
+        }
+
+        return $this->viewRepository->get($from, $size, $orderField, $orderDirection, $searchValue);
+    }
+
+    public function getEntityName(): string
+    {
+        return 'view';
+    }
+
+    public function count(string $searchValue = '', $context = null): int
+    {
+        if (null !== $context) {
+            throw new \RuntimeException('Unexpected not null context');
+        }
+
+        return $this->viewRepository->counter($searchValue);
+    }
+
+    public function update(View $view): void
+    {
+        if (0 === $view->getOrderKey()) {
+            $view->setOrderKey($this->viewRepository->counter() + 1);
+        }
+        $encoder = new Encoder();
+        $name = $view->getName();
+        $webalized = $encoder->webalize($name);
+        if (null === $webalized) {
+            throw new \RuntimeException('Unexpected null webalized name');
+        }
+        $view->setName($webalized);
+        $this->viewRepository->create($view);
+    }
+
+    /**
+     * @param string[] $ids
+     */
+    public function reorderByIds(array $ids): void
+    {
+        $counter = 1;
+        foreach ($ids as $id) {
+            $channel = $this->viewRepository->getById($id);
+            $channel->setOrderKey($counter++);
+            $this->viewRepository->create($channel);
+        }
+    }
+
+    /**
+     * @param string[] $ids
+     */
+    public function deleteByIds(array $ids): void
+    {
+        foreach ($this->viewRepository->getByIds($ids) as $view) {
+            $this->delete($view);
+        }
+    }
+
+    public function delete(View $view): void
+    {
+        $name = $view->getName();
+        $this->viewRepository->delete($view);
+        $this->logger->warning('log.service.view.delete', [
+            'name' => $name,
+        ]);
+    }
+}

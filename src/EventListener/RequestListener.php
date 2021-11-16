2164a864abc59ad11f2dad17757eebf38c8d08e9
Mathieu De Keyzer <me@theus.be>
2021-11-16 22:33:55 +0100

    feat: redirect on login form in case of anonymous user with AccessDeniedHttpException

--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -17,6 +17,7 @@ use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Twig\Environment as TwigEnvironment;
@@ -99,6 +100,12 @@ class RequestListener
                 ]);
                 $response = new RedirectResponse($this->router->generate('notifications.list', [
                     ]));
+                $event->setResponse($response);
+            }
+            if ($exception instanceof AccessDeniedHttpException && null === $event->getRequest()->getUser()) {
+                $response = new RedirectResponse($this->router->generate('fos_user_security_login', [
+                    '_target_path' => $event->getRequest()->getRequestUri(),
+                ]));
                 $event->setResponse($response);
             }
         } catch (Exception $e) {

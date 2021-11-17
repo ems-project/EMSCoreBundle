dea43e3664d09084699eebbf354a0441518d93e6
Mathieu De Keyzer <me@theus.be>
2021-11-17 08:44:59 +0100

    feat: initialize the draft_save_date with the created date (for draft)

---
+++ src/Resources/DoctrineMigrations/pdo_pgsql/Version20211117072146.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20211117072146 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('UPDATE revision SET draft_save_date = created WHERE draft = TRUE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+    }
+}

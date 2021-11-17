878e250f17b85ce9e652428ac4a653ed41d2e39b
Mathieu De Keyzer <me@theus.be>
2021-11-17 09:14:09 +0100

    feat: other doctrine scripts

---
+++ src/Resources/DoctrineMigrations/pdo_mysql/Version20211117080735.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20211117080735 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE revision ADD draft_save_date DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE revision DROP draft_save_date');
+    }
+}

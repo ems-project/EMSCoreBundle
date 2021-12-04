ccc93fb51175e3e8341e98d24019ccad73fe7e44
Mathieu De Keyzer <me@theus.be>
2021-12-04 06:39:17 +0100

    feat: discard draft command (#865)

---
+++ src/Command/Revision/DiscardDraftCommand.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Command\Revision;
+
+use EMS\CommonBundle\Common\Command\AbstractCommand;
+use EMS\CoreBundle\Commands;
+use EMS\CoreBundle\Service\DataService;
+use EMS\CoreBundle\Service\Revision\RevisionService;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DiscardDraftCommand extends AbstractCommand
+{
+    private const ARGUMENT_CONTENT_TYPES = 'content-types';
+    private const OPTION_FORCE = 'force';
+    private const DISCARD_DRAFT_COMMAND_USER = 'DISCARD_DRAFT_COMMAND_USER';
+    protected static $defaultName = Commands::REVISION_DISCARD_DRAFT;
+    private DataService $dataService;
+    private RevisionService $revisionService;
+    /**
+     * @var string[]
+     */
+    private array $contentTypes;
+    private bool $force;
+
+    public function __construct(DataService $dataService, RevisionService $revisionService)
+    {
+        parent::__construct();
+        $this->dataService = $dataService;
+        $this->revisionService = $revisionService;
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription('Discard drafts for content types')
+            ->addArgument(self::ARGUMENT_CONTENT_TYPES, InputArgument::IS_ARRAY, 'ContentType names')
+            ->addOption(self::OPTION_FORCE, null, InputOption::VALUE_NONE, 'Also discard drafts with auto-saved content');
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        parent::initialize($input, $output);
+        $this->contentTypes = $this->getArgumentStringArray(self::ARGUMENT_CONTENT_TYPES);
+        $this->force = $this->getOptionBool(self::OPTION_FORCE);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->io->title('EMSCO - Revision - Discard drafts');
+        foreach ($this->contentTypes as $contentType) {
+            $this->io->section(\sprintf('Discard %s drafts', $contentType));
+            $this->discardDraftFor($contentType);
+        }
+
+        return parent::EXECUTE_SUCCESS;
+    }
+
+    private function discardDraftFor(string $contentType): void
+    {
+        foreach ($this->revisionService->findAllDraftsByContentTypeName($contentType) as $revision) {
+            if (null !== $revision->getDraftSaveDate() && !$this->force) {
+                continue;
+            }
+            $this->dataService->discardDraft($revision, true, self::DISCARD_DRAFT_COMMAND_USER);
+        }
+    }
+}

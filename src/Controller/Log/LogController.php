23712902367be2a08c457892215dbe8b48d0d851
Mathieu De Keyzer <me@theus.be>
2022-01-08 10:12:14 +0100

    feat: add impersonator

--- src/Controller/Log/LogController.php
+++ src/Controller/Log/LogController.php
@@ -9,6 +9,7 @@ use EMS\CoreBundle\Core\Log\LogManager;
 use EMS\CoreBundle\Form\Data\DatetimeTableColumn;
 use EMS\CoreBundle\Form\Data\EntityTable;
 use EMS\CoreBundle\Form\Data\TableAbstract;
+use EMS\CoreBundle\Form\Data\UserTableColumn;
 use EMS\CoreBundle\Form\Form\TableType;
 use EMS\CoreBundle\Helper\DataTableRequest;
 use EMS\CoreBundle\Routes;
@@ -89,7 +90,8 @@ class LogController extends AbstractController
         $table->addColumn('log.index.column.channel', 'channel');
         $table->addColumn('log.index.column.level_name', 'levelName');
         $table->addColumn('log.index.column.message', 'message');
-        $table->addColumn('log.index.column.username', 'username');
+        $table->addColumnDefinition(new UserTableColumn('log.index.column.username', 'username'));
+        $table->addColumnDefinition(new UserTableColumn('log.index.column.impersonator', 'impersonator'));
         $table->addItemGetAction(Routes::LOG_VIEW, 'view.actions.view', 'eye');
         $table->addItemPostAction(Routes::LOG_DELETE, 'view.actions.delete', 'trash', 'view.actions.delete_confirm')->setButtonType('outline-danger');
         $table->addTableAction(TableAbstract::DELETE_ACTION, 'fa fa-trash', 'log.actions.delete_selected', 'log.actions.delete_selected_confirm')

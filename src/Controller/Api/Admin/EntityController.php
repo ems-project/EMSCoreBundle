05f058838c76d23dc8834cd70685d4c5c20395d3
Mathieu De Keyzer <me@theus.be>
2022-02-05 23:59:10 +0100

    ref: EntityController

--- src/Controller/Api/Admin/EntityController.php
+++ src/Controller/Api/Admin/EntityController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace EMS\CoreBundle\Controller\Api\Config;
+namespace EMS\CoreBundle\Controller\Api\Admin;
 
 use EMS\CoreBundle\Service\ContentTypeService;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class ContentTypeController
+class EntityController
 {
     private ContentTypeService $contentTypeService;
 
@@ -19,7 +19,7 @@ class ContentTypeController
         $this->contentTypeService = $contentTypeService;
     }
 
-    public function index(): Response
+    public function index(string $entity): Response
     {
         $names = [];
         foreach ($this->contentTypeService->getAll() as $contentType) {
@@ -32,7 +32,7 @@ class ContentTypeController
         return new JsonResponse($names);
     }
 
-    public function get(string $name): Response
+    public function get(string $entity, string $name): Response
     {
         $contentType = $this->contentTypeService->getByName($name);
         if (false === $contentType) {
@@ -42,7 +42,7 @@ class ContentTypeController
         return new JsonResponse($contentType);
     }
 
-    public function update(string $name, Request $request): Response
+    public function update(string $entity, string $name, Request $request): Response
     {
         $contentType = $this->contentTypeService->getByName($name);
         if (false === $contentType) {

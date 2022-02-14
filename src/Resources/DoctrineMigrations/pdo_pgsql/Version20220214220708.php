6a31bfe32208a107ac7ade56ba6ed4c5cf98255d
Mathieu De Keyzer <me@theus.be>
2022-02-14 23:17:06 +0100

    chore: unique dashboard name

---
+++ src/Resources/DoctrineMigrations/pdo_pgsql/Version20220214220708.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20220214220708 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_5C94FFF85E237E06 ON dashboard (name)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf('postgresql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_5C94FFF85E237E06');
+    }
+}

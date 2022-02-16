abde46a82b0f832441d215bbd935c63bda52f4e3
Mathieu De Keyzer <me@theus.be>
2022-02-16 10:39:35 +0100

    feat: Filter implements EntityServiceInterface (#963)
    * feat: Filter implements EntityServiceInterface
    
    * fix: typo
    
    Co-authored-by: mdk <mathieu.dekeyzer@smals.be>

--- src/Repository/FilterRepository.php
+++ src/Repository/FilterRepository.php
@@ -4,6 +4,7 @@ namespace EMS\CoreBundle\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Registry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use EMS\CoreBundle\Entity\Filter;
 
 class FilterRepository extends ServiceEntityRepository
@@ -29,5 +30,51 @@ class FilterRepository extends ServiceEntityRepository
     {
         $this->getEntityManager()->persist($filter);
         $this->getEntityManager()->flush();
+    }
+
+    /**
+     * @return Filter[]
+     */
+    public function get(int $from, int $size, ?string $orderField, string $orderDirection, string $searchValue): array
+    {
+        $qb = $this->createQueryBuilder('filter')
+            ->setFirstResult($from)
+            ->setMaxResults($size);
+        $this->addSearchFilters($qb, $searchValue);
+
+        if (\in_array($orderField, ['label', 'name'])) {
+            $qb->orderBy(\sprintf('filter.%s', $orderField), $orderDirection);
+        } else {
+            $qb->orderBy('filter.orderKey', $orderDirection);
+        }
+
+        return $qb->getQuery()->execute();
+    }
+
+    public function counter(string $searchValue = ''): int
+    {
+        $qb = $this->createQueryBuilder('filter');
+        $qb->select('count(filter.id)');
+        $this->addSearchFilters($qb, $searchValue);
+
+        return \intval($qb->getQuery()->getSingleScalarResult());
+    }
+
+    public function delete(Filter $filter): void
+    {
+        $this->getEntityManager()->remove($filter);
+        $this->getEntityManager()->flush();
+    }
+
+    private function addSearchFilters(QueryBuilder $qb, string $searchValue): void
+    {
+        if (\strlen($searchValue) > 0) {
+            $or = $qb->expr()->orX(
+                $qb->expr()->like('filter.label', ':term'),
+                $qb->expr()->like('filter.name', ':term'),
+            );
+            $qb->andWhere($or)
+                ->setParameter(':term', '%'.$searchValue.'%');
+        }
     }
 }

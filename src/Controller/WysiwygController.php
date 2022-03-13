cd64396d9de350b6a66dabcb472972309039ee79
Mathieu De Keyzer <me@theus.be>
2022-03-13 15:40:20 +0100

    refactor: class WysiwygController extends AbstractController's routes

--- src/Controller/WysiwygController.php
+++ src/Controller/WysiwygController.php
@@ -14,11 +14,9 @@ use EMS\CoreBundle\Service\WysiwygStylesSetService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\ClickableInterface;
 use Symfony\Component\Form\FormError;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class WysiwygController extends AbstractController

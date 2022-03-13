98cd7855c4d128edcde581c26ee1619d2ce4b006
Mathieu De Keyzer <me@theus.be>
2022-03-13 17:28:59 +0100

    refactor: IndexController

--- src/Controller/ContentManagement/IndexController.php
+++ src/Controller/ContentManagement/IndexController.php
@@ -2,35 +2,21 @@
 
 namespace EMS\CoreBundle\Controller\ContentManagement;
 
-use EMS\CommonBundle\Twig\RequestRuntime;
-use EMS\CoreBundle\Controller\AppController;
 use EMS\CoreBundle\Service\IndexService;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\Form\FormRegistryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController extends AppController
+class IndexController extends AbstractController
 {
-    /** @var IndexService */
-    private $indexService;
+    private IndexService $indexService;
 
     public function __construct(
-        IndexService $indexService,
-        LoggerInterface $logger,
-        FormRegistryInterface $formRegistry,
-        RequestRuntime $requestRuntime
+        IndexService $indexService
     ) {
-        parent::__construct($logger, $formRegistry, $requestRuntime);
         $this->indexService = $indexService;
     }
 
-    /**
-     * @return RedirectResponse
-     *
-     * @Route("/indexes/delete-orphans", name="ems_delete_ophean_indexes", methods={"POST"})
-     */
-    public function deleteOrphansIndexesAction()
+    public function deleteOrphansIndexesAction(): RedirectResponse
     {
         $this->indexService->deleteOrphanIndexes();
 

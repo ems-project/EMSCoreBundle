45c4ac3deaf605ff854bc66bf7086a0962cb7760
Isabelle Michiels <isabelle.michiels@smals.be>
2022-03-17 15:25:17 +0100

    refact

--- src/Command/Release/CreateReleaseCommand.php
+++ src/Command/Release/CreateReleaseCommand.php
@@ -25,6 +25,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+
 class CreateReleaseCommand extends AbstractCommand
 {
     protected static $defaultName = Commands::RELEASE_CREATE;

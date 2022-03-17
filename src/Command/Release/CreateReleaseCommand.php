afa2441e40b7f55ebe23f84255cc31bce2fe2387
Isabelle Michiels <isabelle.michiels@smals.be>
2022-03-17 15:34:27 +0100

    phpcs

--- src/Command/Release/CreateReleaseCommand.php
+++ src/Command/Release/CreateReleaseCommand.php
@@ -25,7 +25,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-
 class CreateReleaseCommand extends AbstractCommand
 {
     protected static $defaultName = Commands::RELEASE_CREATE;

e264f542aacbae56e85f443c6d8c54815d8651a4
Mathieu De Keyzer <me@theus.be>
2022-03-19 18:01:31 +0100

    refactor: routes

--- src/Controller/ContentManagement/FileController.php
+++ src/Controller/ContentManagement/FileController.php
@@ -17,7 +17,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\Annotation\Route;
 
 class FileController extends AbstractController
 {
@@ -30,30 +29,16 @@ class FileController extends AbstractController
         $this->logger = $logger;
     }
 
-    /**
-     * @Route("/data/file/view/{sha1}" , name="ems.file.view", methods={"GET","HEAD"})
-     * @Route("/data/file/view/{sha1}" , name="ems_file_view", methods={"GET","HEAD"})
-     * @Route("/api/file/view/{sha1}" , name="ems.api.file.view", methods={"GET","HEAD"})
-     */
     public function viewFileAction(string $sha1, Request $request): Response
     {
         return $this->fileService->getStreamResponse($sha1, ResponseHeaderBag::DISPOSITION_INLINE, $request);
     }
 
-    /**
-     * @Route("/public/file/{sha1}" , name="ems_file_download_public", methods={"GET","HEAD"})
-     * @Route("/data/file/{sha1}" , name="file.download", methods={"GET","HEAD"})
-     * @Route("/data/file/{sha1}" , name="ems_file_download", methods={"GET","HEAD"})
-     * @Route("/api/file/{sha1}" , name="file.api.download", methods={"GET","HEAD"})
-     */
     public function downloadFileAction(string $sha1, Request $request): Response
     {
         return $this->fileService->getStreamResponse($sha1, ResponseHeaderBag::DISPOSITION_ATTACHMENT, $request);
     }
 
-    /**
-     * @Route("/admin/file/{id}/delete" , name="ems_file_soft_delete", methods={"POST","HEAD"})
-     */
     public function softDeleteFileAction(Request $request, string $id): Response
     {
         if (!$this->isGranted('ROLE_ADMIN')) {
@@ -64,17 +49,11 @@ class FileController extends AbstractController
         return $this->redirectToRoute('ems_core_uploaded_file_logs');
     }
 
-    /**
-     * @Route("/data/file/extract/forced/{sha1}.{_format}" , name="ems_file_extract_forced", defaults={"_format" = "json"}, methods={"GET","HEAD"})
-     */
     public function extractFileContentForced(AssetExtractorService $assetExtractorService, Request $request, string $sha1): Response
     {
         return $this->extractFileContent($assetExtractorService, $request, $sha1, true);
     }
 
-    /**
-     * @Route("/data/file/extract/{sha1}.{_format}" , name="ems_file_extract", defaults={"_format" = "json"}, methods={"GET","HEAD"})
-     */
     public function extractFileContent(AssetExtractorService $assetExtractorService, Request $request, string $sha1, bool $forced = false): Response
     {
         if ($request->hasSession()) {
@@ -101,17 +80,9 @@ class FileController extends AbstractController
     }
 
     /**
-     * @param string $sha1
-     * @param int    $size
-     *
-     * @return Response
-     *
-     * @Route("/data/file/init-upload/{sha1}/{size}" , name="file.init-upload", defaults={"_format" = "json", "apiRoute"=false}, methods={"POST"})
-     * @Route("/api/file/init-upload/{sha1}/{size}" , name="file.api.init-upload", defaults={"_format" = "json", "apiRoute"=true}, methods={"POST"})
-     * @Route("/data/file/init-upload" , name="emsco_file_data_init_upload", defaults={"_format" = "json", "sha1" = null, "size" = null, "apiRoute"=false}, methods={"POST"})
-     * @Route("/api/file/init-upload" , name="emsco_file_api_init_upload", defaults={"_format" = "json", "sha1" = null, "size" = null, "apiRoute"=true}, methods={"POST"})
+     * @param int $size
      */
-    public function initUploadFileAction($sha1, $size, bool $apiRoute, Request $request)
+    public function initUploadFileAction(string $sha1, $size, bool $apiRoute, Request $request): Response
     {
         if ($sha1 || $size) {
             @\trigger_error('You should use the routes emsco_file_data_init_upload or emsco_file_api_init_upload which doesn\'t require url parameters', E_USER_DEPRECATED);
@@ -156,22 +127,16 @@ class FileController extends AbstractController
     }
 
     /**
-     * @param string $sha1
-     * @param string $hash
-     * @param bool   $apiRoute
-     *
-     * @return Response
-     *
-     * @Route("/data/file/upload-chunk/{sha1}", name="file.uploadchunk", defaults={"_format" = "json", "hash" = null, "apiRoute"=false}, methods={"POST"})
-     * @Route("/api/file/upload-chunk/{sha1}", name="file.api.uploadchunk", defaults={"_format" = "json", "hash" = null, "apiRoute"=true}, methods={"POST"})
-     * @Route("/data/file/chunk/{hash}", name="emsco_file_data_chunk_upload", defaults={"_format" = "json", "sha1" = null, "apiRoute"=false}, methods={"POST"})
-     * @Route("/api/file/chunk/{hash}", name="emsco_file_api_chunk_upload", defaults={"_format" = "json", "sha1" = null, "apiRoute"=true}, methods={"POST"})
+     * @param bool $apiRoute
      */
-    public function uploadChunkAction($sha1, $hash, $apiRoute, Request $request)
+    public function uploadChunkAction(?string $sha1, ?string $hash, $apiRoute, Request $request): Response
     {
-        if ($sha1) {
+        if (null === $sha1) {
             $hash = $sha1;
             @\trigger_error('You should use the routes emsco_file_data_chunk_upload or emsco_file_api_chunk_upload which use a hash parameter', E_USER_DEPRECATED);
+        }
+        if (null === $hash) {
+            throw new \RuntimeException('Unexpected null hash');
         }
 
         $chunk = $request->getContent();
@@ -202,13 +167,7 @@ class FileController extends AbstractController
         ]);
     }
 
-    /**
-     * @return Response
-     *
-     * @Route("/images/index", name="ems_images_index", defaults={"_format"="json"}, methods={"GET", "HEAD"})
-     * @Route("/api/images", name="ems_api_images_index", defaults={"_format"="json"}, methods={"GET", "HEAD"})
-     */
-    public function indexImagesAction()
+    public function indexImagesAction(): Response
     {
         $images = $this->fileService->getImages();
 
@@ -217,13 +176,7 @@ class FileController extends AbstractController
         ]);
     }
 
-    /**
-     * @return Response
-     *
-     * @Route("/file/upload", name="ems_image_upload_url", defaults={"_format"="json"}, methods={"POST"})
-     * @Route("/api/file", name="ems_api_image_upload_url", defaults={"_format"="json"}, methods={"POST"})
-     */
-    public function uploadFileAction(Request $request)
+    public function uploadFileAction(Request $request): Response
     {
         /** @var UploadedFile $file */
         $file = $request->files->get('upload');

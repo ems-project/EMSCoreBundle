72dc67980337014e2f5b2bf8667e273dd520fd4c
Mathieu De Keyzer <me@theus.be>
2022-03-19 22:43:16 +0100

    refactor: DefaultController.php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,23 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    /**
-     * @return Response
-     * @Route("/documentation", name="documentation")
-     */
-    public function documentationAction()
+    public function documentation(): Response
     {
-        // replace this example code with whatever you need
         return $this->render('@EMSCore/default/documentation.html.twig');
-    }
-
-    /**
-     * @return Response
-     * @Route("/coming-soon", name="coming-soon")
-     */
-    public function comingSoonAction()
-    {
-        // replace this example code with whatever you need
-        return $this->render('@EMSCore/default/coming-soon.html.twig');
     }
 }

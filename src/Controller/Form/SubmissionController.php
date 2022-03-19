fbd9a1d57bf12df1644a47cf7e210bcb08ce712d
Mathieu De Keyzer <me@theus.be>
2022-03-19 23:56:48 +0100

    chore: phpcs

--- src/Controller/Form/SubmissionController.php
+++ src/Controller/Form/SubmissionController.php
@@ -21,8 +21,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
-use Symfony\Component\HttpFoundation\StreamedResponse;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 

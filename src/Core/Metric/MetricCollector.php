4fa5798ab99741c55ff946b09608925790651727
AdrienHq <34314047+AdrienHq@users.noreply.github.com>
2022-03-23 14:22:36 +0100

    feat: Metrics for healthStatus / Revision counter / Content Type counter

--- src/Core/Metric/MetricCollector.php
+++ src/Core/Metric/MetricCollector.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace EMS\CoreBundle\Core\Metric;
 
 use EMS\CommonBundle\Contracts\Metric\EMSMetricsCollectorInterface;
+use EMS\CommonBundle\Service\ElasticaService;
 use EMS\CoreBundle\Repository\ContentTypeRepository;
 use EMS\CoreBundle\Repository\RevisionRepository;
+use EMS\CoreBundle\Service\EnvironmentService;
 use Prometheus\CollectorRegistry;
 use Prometheus\Exception\MetricNotFoundException;
 
@@ -14,11 +16,15 @@ class MetricCollector implements EMSMetricsCollectorInterface
 {
     private RevisionRepository $revisionRepository;
     private ContentTypeRepository $contentTypeRepository;
+    private ElasticaService $elasticaService;
+    private EnvironmentService $environmentService;
 
-    public function __construct(RevisionRepository $revisionRepository, ContentTypeRepository $contentTypeRepository)
+    public function __construct(RevisionRepository $revisionRepository, ContentTypeRepository $contentTypeRepository, ElasticaService $elasticaService, EnvironmentService $environmentService)
     {
         $this->revisionRepository = $revisionRepository;
         $this->contentTypeRepository = $contentTypeRepository;
+        $this->elasticaService = $elasticaService;
+        $this->environmentService = $environmentService;
     }
 
     /**
@@ -26,12 +32,44 @@ class MetricCollector implements EMSMetricsCollectorInterface
      */
     public function collect(CollectorRegistry $registry): void
     {
-        $registry = $registry->getOrRegisterGauge(
-            'EMSCore',
-            'Content_Type_Counter',
-            'The number of content type',
-            ['numberOfCt']
+        $status = $this->elasticaService->getHealthStatus('green', '15s');
+        $boolStatus = 0;
+        if ($status === "green" or $status === "yellow" ){
+            $boolStatus = 3;
+        }elseif ($status === "yellow"){
+            $boolStatus = 2;
+        }
+
+        $healthStatus = $registry->getOrRegisterGauge(
+            'emsco',
+            'elasticms_status',
+            'Health status of the cluster',
+            ['status']
         );
-        $registry->set(($this->contentTypeRepository->count([])), ['ctCounter']);
+        $healthStatus->set($boolStatus, [$status]);
+
+        $envs = $this->environmentService->getEnvironments();
+        $count = $registry->getOrRegisterGauge(
+            'emsco',
+            'count_total_',
+            'The number of Content Type',
+            ['env']
+        );
+        foreach($envs as $env){
+            $totalContentTypeByEnv = $this->contentTypeRepository->countContentTypeByEnvironment($env->getId());
+            $count->set(floatval($totalContentTypeByEnv), [$env->getName()]);
+        }
+
+        $documentCounter = $registry->getOrRegisterGauge(
+            'emsco',
+            'revisions_total',
+            'Number of document by Content Type',
+            ['contenttype']
+        );
+        $allContentTypes = $this->contentTypeRepository->findAll();
+        foreach($allContentTypes as $cT ){
+            $revision = $this->revisionRepository->findByContentType($cT);
+            $documentCounter->set(count($revision), [$cT->getName()]);
+        }
     }
 }

3f8dc0b9078cdd5571d0704cde49f30b7445f957
AdrienHq <34314047+AdrienHq@users.noreply.github.com>
2022-04-06 13:59:53 +0200

    feat: pgsql migration

---
+++ src/Resources/DoctrineMigrations/pdo_pgsql/Version20220406115340.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20220406115340 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE "user" ADD settings TEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE "user" DROP settings');
+    }
+}

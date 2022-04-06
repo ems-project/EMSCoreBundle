26bfa72715c570b43816bdd9d478affe68226eab
AdrienHq <34314047+AdrienHq@users.noreply.github.com>
2022-04-06 13:58:55 +0200

    feat: UserType settings

--- src/Form/Form/UserType.php
+++ src/Form/Form/UserType.php
@@ -95,6 +95,12 @@ final class UserType extends AbstractType
                 'language' => 'ace/mode/json',
                 'attr' => ['class' => 'wysiwyg-profile-options'],
             ])
+            ->add('settings', CodeEditorType::class, [
+                'label' => 'user.settings',
+                'required' => false,
+                'language' => 'ace/mode/json',
+                'attr' => ['class' => 'settings-profile'],
+            ])
             ->add('roles', ChoiceType::class, [
                 'choices' => $this->userService->getExistingRoles(),
                 'label' => 'Roles',

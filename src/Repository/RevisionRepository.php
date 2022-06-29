643c3e6e9da739dffaa3e279e6ba34c5f8c7e98a
David mattei <d.mattei89@gmail.com>
2022-06-29 15:33:23 +0200

    feat: unpublish command (#1134)
    * ref: abstractEnvironmentCommand
    
    * feat: unpublish command
    
    * fix: options lockBy, until not used in function search
    
    * fix: searchByEnvironmentOuuids do not load envs
    
    Adding the "re" relationship in the select is telling doctrine you only want that 1 environment. We may need all environments!
    
    * feat: add extra logic exception require 1 environment.
    
    A revision always needs at least 1 environment!
    
    * feat: dry-un for align & unpublish environment
    
    * feat: docs

--- src/Repository/RevisionRepository.php
+++ src/Repository/RevisionRepository.php
@@ -121,6 +121,7 @@ class RevisionRepository extends EntityRepository
     {
         $qb = $this->createQueryBuilder('r');
         $qb
+            ->addSelect('ce, c')
             ->join('r.contentType', 'c')
             ->join('c.environment', 'ce')
             ->join('r.environments', 're')

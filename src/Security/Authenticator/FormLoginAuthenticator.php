9e6f970caadf7ca57fdf435bdcbfadae8c5c74a2
David mattei <d.mattei89@gmail.com>
2022-08-20 09:35:44 +0200

    phpcs

--- src/Security/Authenticator/FormLoginAuthenticator.php
+++ src/Security/Authenticator/FormLoginAuthenticator.php
@@ -11,7 +11,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Security\Core\Exception\BadCredentialsException;
 use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;

7cb88bd41fc4879e1b662850329b37d4fdd384f5
David mattei <d.mattei89@gmail.com>
2022-08-29 11:15:53 +0200

    chore(es7): es service only es7
    Removed convertMapping not used anywhere

--- src/Service/ElasticsearchService.php
+++ src/Service/ElasticsearchService.php
@@ -19,63 +19,13 @@ class ElasticsearchService
     }
 
     /**
-     * Compare the parameter specified version with a string.
-     *
-     * @return mixed
-     */
-    public function compare(string $version)
-    {
-        return \version_compare($this->getVersion(), $version);
-    }
-
-    /**
      * Return a keyword mapping (not analyzed).
      *
      * @return array<string, string>
      */
     public function getKeywordMapping(): array
     {
-        if (\version_compare($this->getVersion(), '5') > 0) {
-            return [
-                'type' => 'keyword',
-            ];
-        }
-
-        return [
-            'type' => 'string',
-            'index' => 'not_analyzed',
-        ];
-    }
-
-    /**
-     * @param array<mixed> $in
-     *
-     * @return string[]
-     */
-    public function convertMapping(array $in): array
-    {
-        $out = $in;
-        if (\version_compare($this->getVersion(), '5') > 0) {
-            if (isset($out['analyzer']) && 'keyword' === $out['analyzer']) {
-                $out['type'] = 'keyword';
-                unset($out['analyzer']);
-                unset($out['fielddata']);
-                unset($out['index']);
-            } elseif (isset($out['index']) && 'not_analyzed' === $out['index']) {
-                $out['type'] = 'keyword';
-                unset($out['analyzer']);
-                unset($out['fielddata']);
-                unset($out['index']);
-            } elseif (isset($out['type']) && 'string' === $out['type']) {
-                $out['type'] = 'text';
-            } elseif (isset($out['type']) && 'keyword' === $out['type']) {
-                unset($out['analyzer']);
-                unset($out['fielddata']);
-                unset($out['index']);
-            }
-        }
-
-        return $out;
+        return ['type' => 'keyword'];
     }
 
     /**
@@ -91,22 +41,20 @@ class ElasticsearchService
             $mapping['copy_to'] = \explode(',', $mapping['copy_to']);
         }
 
-        if (\version_compare($this->getVersion(), '5') > 0) {
-            if ('string' === $mapping['type']) {
-                if ((isset($mapping['analyzer']) && 'keyword' === $mapping['analyzer']) || (empty($mapping['analyzer']) && isset($mapping['index']) && 'not_analyzed' === $mapping['index'])) {
-                    $mapping['type'] = 'keyword';
-                    unset($mapping['analyzer']);
-                } else {
-                    $mapping['type'] = 'text';
-                }
+        if ('string' === $mapping['type']) {
+            if ((isset($mapping['analyzer']) && 'keyword' === $mapping['analyzer']) || (empty($mapping['analyzer']) && isset($mapping['index']) && 'not_analyzed' === $mapping['index'])) {
+                $mapping['type'] = 'keyword';
+                unset($mapping['analyzer']);
+            } else {
+                $mapping['type'] = 'text';
             }
+        }
 
-            if (isset($mapping['index']) && 'No' === $mapping['index']) {
-                $mapping['index'] = false;
-            }
-            if (isset($mapping['index']) && false !== $mapping['index']) {
-                $mapping['index'] = true;
-            }
+        if (isset($mapping['index']) && 'No' === $mapping['index']) {
+            $mapping['index'] = false;
+        }
+        if (isset($mapping['index']) && false !== $mapping['index']) {
+            $mapping['index'] = true;
         }
 
         return $mapping;
@@ -153,13 +101,5 @@ class ElasticsearchService
         return [
             'type' => 'long',
         ];
-    }
-
-    /**
-     * @return mixed
-     */
-    public function withAllMapping()
-    {
-        return \version_compare($this->getVersion(), '5.6') < 0;
     }
 }

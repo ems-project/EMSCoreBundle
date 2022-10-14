ems-project/EMSCoreBundle

commit 9ecb49be652690e77e2401700f402452dc97cf9d

fix: phpstan 1.8 with sf & doctrine plugin (#1165)
- Fix unused injections
- Add new CreatedModifiedTrait.php for entities
- Fix repositories describe doctrine methods (find, findBy, ...)

src/Form/Field/ObjectPickerType.php
@@ -28,15 +28,15 @@ class ObjectPickerType extends Select2Type
             'sortable' => false,
             'with_warning' => true,
             'choice_loader' => function (Options $options) {
-                $loadAll = $options->offsetGet('dynamicLoading') ? false : true;
-                $circleOnly = $options->offsetGet('circle-only');
-                $withWarning = $options->offsetGet('with_warning');
-                $querySearch = $options->offsetGet('querySearch');
+                $loadAll = $options['dynamicLoading'];
+                $circleOnly = $options['circle-only'];
+                $withWarning = $options['with_warning'];
+                $querySearch = $options['querySearch'];
                 if (!\is_string($querySearch) || 0 === \strlen($querySearch)) {
                     $querySearch = null;
                 }
 
-                return $this->choiceListFactory->createLoader($options->offsetGet('type'), $loadAll, $circleOnly, $withWarning, $querySearch);
+                return $this->choiceListFactory->createLoader($options['type'], $loadAll, $circleOnly, $withWarning, $querySearch);
             },
             'choice_label' => function ($value, $key, $index) {
                 return $value->getLabel();

commit b09c1ad4b54e314627187e31776b3e310ec804bd

fix(search): load all for type not working anymore.
Bug introduced in 9ecb49be652690e77e2401700f402452dc97cf9d

src/Form/Field/ObjectPickerType.php
@@ -28,7 +28,7 @@ class ObjectPickerType extends Select2Type
             'sortable' => false,
             'with_warning' => true,
             'choice_loader' => function (Options $options) {
-                $loadAll = $options['dynamicLoading'];
+                $loadAll = !$options['dynamicLoading'];
                 $circleOnly = $options['circle-only'];
                 $withWarning = $options['with_warning'];
                 $querySearch = $options['querySearch'];
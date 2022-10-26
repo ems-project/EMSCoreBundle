ems-project/EMSCoreBundle

commit dab5b1e8b023d4acd46dfa0492acb02ee67e14d0

feat(user): options and simplied UI (#1224)

src/Form/Form/RevisionType.php
@@ -3,6 +3,8 @@
 namespace EMS\CoreBundle\Form\Form;
 
 use EMS\CoreBundle\Core\ContentType\Version\VersionOptions;
+use EMS\CoreBundle\Core\User\UserManager;
+use EMS\CoreBundle\Core\User\UserOptions;
 use EMS\CoreBundle\DependencyInjection\EMSCoreExtension;
 use EMS\CoreBundle\Entity\ContentType;
 use EMS\CoreBundle\Entity\Environment;
@@ -21,10 +23,12 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class RevisionType extends AbstractType
 {
     private FormRegistryInterface $formRegistry;
+    private UserManager $userManager;
 
-    public function __construct(FormRegistryInterface $formRegistry)
+    public function __construct(FormRegistryInterface $formRegistry, UserManager $userManager)
     {
         $this->formRegistry = $formRegistry;
+        $this->userManager = $userManager;
     }
 
     /**
@@ -35,6 +39,8 @@ class RevisionType extends AbstractType
     {
         /** @var Revision|null $revision */
         $revision = $builder->getData();
+        $user = $this->userManager->getAuthenticatedUser();
+        $simplifiedUI = $user->getUserOptions()->isEnabled(UserOptions::SIMPLIFIED_UI);
         $contentType = $revision ? $revision->giveContentType() : $options['content_type'];
 
         if (!$contentType instanceof ContentType) {
@@ -54,11 +60,13 @@ class RevisionType extends AbstractType
 
         if ($revision) {
             if ($revision->getDraft()) {
-                $builder->add('save', SubmitEmsType::class, [
-                    'label' => 'form.form.revision-type.save-draft-label',
-                    'attr' => ['class' => 'btn btn-default btn-sm'],
-                    'icon' => 'fa fa-save',
-                ]);
+                if (!$simplifiedUI) {
+                    $builder->add('save', SubmitEmsType::class, [
+                        'label' => 'form.form.revision-type.save-draft-label',
+                        'attr' => ['class' => 'btn btn-default btn-sm'],
+                        'icon' => 'fa fa-save',
+                    ]);
+                }
             } else {
                 $publishedEnvironmentLabels = $revision->getEnvironments()->map(fn (Environment $e) => $e->getLabel());
                 if (\count($publishedEnvironmentLabels) > 0) {
@@ -84,24 +92,26 @@ class RevisionType extends AbstractType
         ->addModelTransformer(new DataFieldModelTransformer($contentType->getFieldType(), $this->formRegistry))
         ->addViewTransformer(new DataFieldViewTransformer($contentType->getFieldType(), $this->formRegistry));
 
-        if ($options['has_clipboard']) {
-            $builder->add('paste', SubmitEmsType::class, [
-                'label' => 'form.form.revision-type.paste-label',
-                'attr' => [
-                    'class' => '',
-                ],
-                'icon' => 'fa fa-paste',
-            ]);
-        }
-
-        if ($options['has_copy']) {
-            $builder->add('copy', SubmitEmsType::class, [
-                    'label' => 'form.form.revision-type.copy-label',
+        if (!$simplifiedUI) {
+            if ($options['has_clipboard']) {
+                $builder->add('paste', SubmitEmsType::class, [
+                    'label' => 'form.form.revision-type.paste-label',
                     'attr' => [
                         'class' => '',
                     ],
-                    'icon' => 'fa fa-copy',
-            ]);
+                    'icon' => 'fa fa-paste',
+                ]);
+            }
+
+            if ($options['has_copy']) {
+                $builder->add('copy', SubmitEmsType::class, [
+                        'label' => 'form.form.revision-type.copy-label',
+                        'attr' => [
+                            'class' => '',
+                        ],
+                        'icon' => 'fa fa-copy',
+                ]);
+            }
         }
 
         if (null !== $revision && $revision->getDraft()) {

src/Core/User/UserOptions.php
@@ -1,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Core\User;
+
+/**
+ * @implements \ArrayAccess<string, bool>
+ */
+class UserOptions implements \ArrayAccess
+{
+    /** @var array<string, bool> */
+    private array $options = [];
+
+    public const SIMPLIFIED_UI = 'simplified_ui';
+
+    /**
+     * @param array<string, bool> $data
+     */
+    public function __construct(array $data)
+    {
+        $this->options[self::SIMPLIFIED_UI] = $data[self::SIMPLIFIED_UI] ?? false;
+    }
+
+    public function isEnabled(string $option): bool
+    {
+        return true === $this->options[$option];
+    }
+
+    /**
+     * @return array<string, bool>
+     */
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    public function offsetExists($offset): bool
+    {
+        return isset($this->options[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->options[$offset] ?? null;
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        $this->options[$offset] = $value;
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->options[$offset]);
+    }
+}

src/Entity/User.php
@@ -5,6 +5,7 @@ namespace EMS\CoreBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use EMS\CoreBundle\Core\User\UserOptions;
 use EMS\CoreBundle\Roles;
 use EMS\Helpers\Standard\DateTime;
 use EMS\Helpers\Standard\Type;
@@ -148,6 +149,13 @@ class User implements UserInterface, EntityInterface, PasswordAuthenticatedUserI
      * @ORM\Column(name="roles", type="array")
      */
     private array $roles;
+
+    /**
+     * @var ?array<string, bool>
+     *
+     * @ORM\Column(name="user_options", type="json", nullable=true)
+     */
+    protected ?array $userOptions = [];
 
     private const DEFAULT_LOCALE = 'en';
 
@@ -592,5 +600,15 @@ class User implements UserInterface, EntityInterface, PasswordAuthenticatedUserI
         }
 
         return $this;
+    }
+
+    public function getUserOptions(): UserOptions
+    {
+        return new UserOptions($this->userOptions ?? []);
+    }
+
+    public function setUserOptions(UserOptions $userOptions): void
+    {
+        $this->userOptions = $userOptions->getOptions();
     }
 }

src/Form/Form/UserOptionsType.php
@@ -1,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Form\Form;
+
+use EMS\CoreBundle\Core\User\UserOptions;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserOptionsType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface<FormBuilderInterface> $builder
+     * @param array<string, mixed>                       $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(UserOptions::SIMPLIFIED_UI, CheckboxType::class, [
+                'required' => false,
+                'label' => 'user.option.simplified_ui',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(['translation_domain' => 'emsco-user']);
+    }
+}

src/Form/Form/UserType.php
@@ -121,6 +121,7 @@ final class UserType extends AbstractType
                 'choices' => \array_flip(Locales::getNames()),
                 'choice_translation_domain' => false,
             ])
+            ->add('userOptions', UserOptionsType::class, ['label' => 'user.option.title'])
         ;
 
         if ($this->circleObject) {

src/Form/User/UserProfileType.php
@@ -9,6 +9,7 @@ use EMS\CoreBundle\EMSCoreBundle;
 use EMS\CoreBundle\Entity\User;
 use EMS\CoreBundle\Entity\WysiwygProfile;
 use EMS\CoreBundle\Form\Field\CodeEditorType;
+use EMS\CoreBundle\Form\Form\UserOptionsType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -50,6 +51,7 @@ class UserProfileType extends AbstractType
             ->add('layoutBoxed', null, ['label' => 'user.layout_boxed'])
             ->add('sidebarMini', null, ['label' => 'user.sidebar_mini'])
             ->add('sidebarCollapse', null, ['label' => 'user.sidebar_collapse'])
+            ->add('userOptions', UserOptionsType::class, ['label' => 'user.option.title'])
             ->add('locale', ChoiceType::class, [
                 'label' => 'user.locale',
                 'translation_domain' => EMSCoreBundle::TRANS_FORM_DOMAIN,
@@ -63,8 +65,7 @@ class UserProfileType extends AbstractType
                 'required' => false,
                 'choices' => \array_flip(Locales::getNames()),
                 'choice_translation_domain' => false,
-            ])
-            ->remove('username');
+            ]);
 
         $builder
             ->add('wysiwygProfile', EntityType::class, [

src/Resources/DoctrineMigrations/pdo_mysql/Version20221020105730.php
@@ -1,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Platforms\MySQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20221020105730 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQLPlatform'."
+        );
+
+        $this->addSql('ALTER TABLE task CHANGE created created DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE task CHANGE created created DATETIME NOT NULL');
+
+        $this->addSql('ALTER TABLE user ADD user_options LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQLPlatform'."
+        );
+
+        $this->addSql('ALTER TABLE task CHANGE created created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+        $this->addSql('ALTER TABLE task CHANGE modified modified DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+
+        $this->addSql('ALTER TABLE `user` DROP user_options');
+    }
+}

src/Resources/DoctrineMigrations/pdo_pgsql/Version20221020105614.php
@@ -1,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20221020105614 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof PostgreSQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQLPlatform'."
+        );
+
+        $this->addSql('ALTER TABLE task ALTER created DROP DEFAULT');
+        $this->addSql('ALTER TABLE task ALTER modified DROP DEFAULT');
+
+        $this->addSql('ALTER TABLE "user" ADD user_options JSON DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof PostgreSQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQLPlatform'."
+        );
+
+        $this->addSql('ALTER TABLE task ALTER created SET DEFAULT CURRENT_TIMESTAMP');
+        $this->addSql('ALTER TABLE task ALTER modified SET DEFAULT CURRENT_TIMESTAMP');
+
+        $this->addSql('ALTER TABLE "user" DROP user_options');
+    }
+}

commit e340db33f5a95b64e4e73b819ebffc360db98713

fix(environment): recompute command broken
Bug introduced in #1224

In UserManager.php line 197:
Token is null, could not get the currentUser from token.

Offcourse CLI, there is no authenticated user

src/Core/User/UserManager.php
@@ -50,6 +50,15 @@ final class UserManager
         return $user;
     }
 
+    public function getUser(): ?User
+    {
+        try {
+            return $this->getAuthenticatedUser();
+        } catch (\Throwable $e) {
+            return null;
+        }
+    }
+
     public function getAuthenticatedUser(): User
     {
         $token = $this->getToken();

src/Form/Form/RevisionType.php
@@ -39,8 +39,8 @@ class RevisionType extends AbstractType
     {
         /** @var Revision|null $revision */
         $revision = $builder->getData();
-        $user = $this->userManager->getAuthenticatedUser();
-        $simplifiedUI = $user->getUserOptions()->isEnabled(UserOptions::SIMPLIFIED_UI);
+        $user = $this->userManager->getUser();
+        $simplifiedUI = $user && $user->getUserOptions()->isEnabled(UserOptions::SIMPLIFIED_UI);
         $contentType = $revision ? $revision->giveContentType() : $options['content_type'];
 
         if (!$contentType instanceof ContentType) {
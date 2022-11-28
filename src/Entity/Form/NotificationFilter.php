920e23827004c589cd8c343d5bd18fefbb580886
David mattei <d.mattei89@gmail.com>
2022-11-28 13:55:42 +0100

    fix: notification dashboard (#137)

--- src/Entity/Form/NotificationFilter.php
+++ src/Entity/Form/NotificationFilter.php
@@ -1,103 +1,53 @@
 <?php
 
+declare(strict_types=1);
+
 namespace EMS\CoreBundle\Entity\Form;
 
 use EMS\CoreBundle\Entity\ContentType;
 use EMS\CoreBundle\Entity\Environment;
 use EMS\CoreBundle\Entity\Template;
 
-/**
- * RebuildIndex.
- */
 class NotificationFilter
 {
     private ?Template $template = null;
-
     private ?Environment $environment = null;
 
     private ?ContentType $contentType = null;
 
-    /**
-     * Set the template filter.
-     *
-     * @param Template $template
-     *
-     * @return NotificationFilter
-     */
-    public function setTemplate($template)
+    public function setTemplate(?Template $template): NotificationFilter
     {
         $this->template = $template;
 
         return $this;
     }
 
-    /**
-     * Get the selected template filter.
-     *
-     * @return Template
-     */
-    public function getTemplate()
+    public function getTemplate(): ?Template
     {
-        if (null === $this->template) {
-            throw new \RuntimeException('Unexpected null template');
-        }
-
         return $this->template;
     }
 
-    /**
-     * Set the $environment filter.
-     *
-     * @param Environment $environment
-     *
-     * @return NotificationFilter
-     */
-    public function setEnvironment($environment)
+    public function setEnvironment(?Environment $environment): NotificationFilter
     {
         $this->environment = $environment;
 
         return $this;
     }
 
-    /**
-     * Get the selected environment filter.
-     *
-     * @return Environment
-     */
-    public function getEnvironment()
+    public function getEnvironment(): ?Environment
     {
-        if (null === $this->environment) {
-            throw new \RuntimeException('Unexpected null environment');
-        }
-
         return $this->environment;
     }
 
-    /**
-     * Set the $contentType filter.
-     *
-     * @param ContentType $contentType
-     *
-     * @return NotificationFilter
-     */
-    public function setContentType($contentType)
+    public function setContentType(?ContentType $contentType): NotificationFilter
     {
         $this->contentType = $contentType;
 
         return $this;
     }
 
-    /**
-     * Get the selected contentType filter.
-     *
-     * @return ContentType
-     */
-    public function getContentType()
+    public function getContentType(): ?ContentType
     {
-        if (null === $this->contentType) {
-            throw new \RuntimeException('Unexpected null contentType');
-        }
-
         return $this->contentType;
     }
 }

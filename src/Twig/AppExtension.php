3648905a931f767dbbdcfa868c32ed2d029856f4
Mathieu De Keyzer <me@theus.be>
2023-04-11 09:30:26 +0200

    refactor(twig): emsch_uuid -> ems_uuid (#426)
    Co-authored-by: David mattei <d.mattei89@gmail.com>

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -100,7 +100,7 @@ class AppExtension extends AbstractExtension
             new TwigFunction('emsco_generate_email', $this->generateEmailMessage(...)),
             new TwigFunction('emsco_send_email', $this->sendEmail(...)),
             new TwigFunction('emsco_users_enabled', [UserRuntime::class, 'getUsersEnabled']),
-            new TwigFunction('emsco_uuid', [Uuid::class, 'uuid4']),
+            new TwigFunction('emsco_uuid', [Uuid::class, 'uuid4'], ['deprecated' => true]),
             new TwigFunction('emsco_datatable', [DatatableRuntime::class, 'generateDatatable'], ['is_safe' => ['html']]),
             new TwigFunction('emsco_datatable_excel_path', [DatatableRuntime::class, 'getExcelPath'], ['is_safe' => ['html']]),
             new TwigFunction('emsco_datatable_csv_path', [DatatableRuntime::class, 'getCsvPath'], ['is_safe' => ['html']]),

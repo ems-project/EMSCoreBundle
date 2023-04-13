8aa9777e9ddeaa01e945e90817c3ece3e41c7474
Mathieu De Keyzer <me@theus.be>
2023-04-13 15:57:31 +0200

    feat(emsco/env): rebuild all flag (#428)
    Co-authored-by: David mattei <d.mattei89@gmail.com>

--- src/Command/RebuildCommand.php
+++ src/Command/RebuildCommand.php
@@ -3,6 +3,7 @@
 namespace EMS\CoreBundle\Command;
 
 use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\Persistence\ObjectManager;
 use EMS\CommonBundle\Service\ElasticaService;
 use EMS\CoreBundle\Entity\ContentType;
 use EMS\CoreBundle\Entity\Environment;
@@ -20,8 +21,14 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class RebuildCommand extends EmsCommand
 {
+    public const ALL = 'all';
     protected static $defaultName = self::COMMAND;
     final public const COMMAND = 'ems:environment:rebuild';
+    private bool $signData;
+    private int $bulkSize;
+    private ObjectManager $em;
+    private bool $yellowOk;
+    private bool $all;
 
     public function __construct(private readonly Registry $doctrine, protected LoggerInterface $logger, private readonly ContentTypeService $contentTypeService, private readonly EnvironmentService $environmentService, private readonly ReindexCommand $reindexCommand, private readonly ElasticaService $elasticaService, private readonly Mapping $mapping, private readonly AliasService $aliasService, private readonly string $instanceId, private readonly string $defaultBulkSize)
     {
@@ -33,8 +40,14 @@ class RebuildCommand extends EmsCommand
         $this->setDescription('Rebuild an environment in a brand new index')
             ->addArgument(
                 'name',
-                InputArgument::REQUIRED,
+                InputArgument::OPTIONAL,
                 'Environment name'
+            )
+            ->addOption(
+                self::ALL,
+                null,
+                InputOption::VALUE_NONE,
+                'Rebuild all managed indexes'
             )
             ->addOption(
                 'yellow-ok',
@@ -67,12 +80,13 @@ class RebuildCommand extends EmsCommand
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->aliasService->build();
-        $yellowOk = true === $input->getOption('yellow-ok');
+        $this->yellowOk = true === $input->getOption('yellow-ok');
+        $this->all = true === $input->getOption(self::ALL);
         $this->formatStyles($output);
-        $this->waitFor($yellowOk, $output);
+        $this->waitFor($this->yellowOk, $output);
 
-        $bulkSize = \intval($input->getOption('bulk-size'));
-        if (0 === $bulkSize) {
+        $this->bulkSize = \intval($input->getOption('bulk-size'));
+        if ($this->bulkSize <= 0) {
             throw new \RuntimeException('Unexpected bulk size option');
         }
 
@@ -81,80 +95,36 @@ class RebuildCommand extends EmsCommand
             $output->writeln('The option --sign-data is deprecated');
         }
 
-        $signData = !$input->getOption('dont-sign');
+        $this->signData = !$input->getOption('dont-sign');
 
-        $em = $this->doctrine->getManager();
+        $this->em = $this->doctrine->getManager();
         $name = $input->getArgument('name');
-        if (!\is_string($name)) {
-            throw new \RuntimeException('Unexpected content type name');
-        }
-
-        $envRepo = $em->getRepository(Environment::class);
+        $envRepo = $this->em->getRepository(Environment::class);
         if (!$envRepo instanceof EnvironmentRepository) {
             throw new \RuntimeException('Unexpected environment repository');
         }
 
-        /** @var Environment|null $environment */
-        $environment = $envRepo->findOneBy(['name' => $name, 'managed' => true]);
+        if (\is_string($name)) {
+            $environment = $envRepo->findOneBy(['name' => $name, 'managed' => true]);
+            if (!$environment instanceof Environment) {
+                $output->writeln('WARNING: Managed environment named '.$name.' not found');
 
-        if (null === $environment) {
-            $output->writeln('WARNING: Environment named '.$name.' not found');
-
-            return -1;
-        }
-
-        if ($environment->getAlias() != $this->instanceId.$environment->getName()) {
-            $environment->setAlias($this->instanceId.$environment->getName());
-            $em->persist($environment);
-            $em->flush();
-            $output->writeln('Alias has been aligned to '.$environment->getAlias());
-        }
-
-        /** @var ContentTypeRepository $contentTypeRepository */
-        $contentTypeRepository = $em->getRepository(ContentType::class);
-        $contentTypes = $contentTypeRepository->findAll();
-
-        $body = $this->environmentService->getIndexAnalysisConfiguration();
-
-        $newIndexName = $environment->getNewIndexName();
-        $this->mapping->createIndex($newIndexName, $body);
-
-        $output->writeln('A new index '.$newIndexName.' has been created');
-        $this->waitFor($yellowOk, $output);
-        $output->writeln(\count($contentTypes).' content types will be re-indexed');
-
-        $countContentType = 1;
-
-        /** @var ContentType $contentType */
-        foreach ($contentTypes as $contentType) {
-            $contentTypeEnvironment = $contentType->getEnvironment();
-            if (null === $contentTypeEnvironment) {
-                throw new \RuntimeException('Unexpected null environment');
+                return -1;
             }
-            if (!$contentType->getDeleted() && $contentType->getEnvironment() && $contentTypeEnvironment->getManaged()) {
-                $this->contentTypeService->updateMapping($contentType, $newIndexName);
-                $output->writeln('A mapping has been defined for '.$contentType->getSingularName());
-                ++$countContentType;
+
+            $this->rebuildEnvironment($environment, $output);
+        } elseif ($this->all) {
+            foreach ($envRepo->findAll() as $environment) {
+                if (!$environment instanceof Environment) {
+                    throw new \RuntimeException('Unexpected environment object');
+                }
+                if (!$environment->getManaged()) {
+                    continue;
+                }
+                $this->rebuildEnvironment($environment, $output);
             }
-        }
-
-        /** @var ContentType $contentType */
-        foreach ($contentTypes as $contentType) {
-            if (!$contentType->getDeleted() && null !== $contentType->getEnvironment() && $contentType->getEnvironment()->getManaged()) {
-                $this->reindexCommand->reindex($name, $contentType, $newIndexName, $output, $signData, $bulkSize);
-                $output->writeln('');
-                $output->writeln($contentType->getPluralName().' have been re-indexed ');
-            }
-        }
-
-        $this->waitFor($yellowOk, $output);
-
-        $atomicSwitch = $this->aliasService->atomicSwitch($environment, $newIndexName);
-
-        foreach ($atomicSwitch as $action) {
-            if (isset($action['add'])) {
-                $output->writeln(\sprintf('The alias <info>%s</info> is now point to : %s', $action['add']['alias'], $action['add']['index']));
-            }
+        } else {
+            throw new \RuntimeException('A content type name argument or the flag --all must be defined');
         }
 
         return 0;
@@ -168,6 +138,65 @@ class RebuildCommand extends EmsCommand
         } else {
             $output->writeln('Waiting for green...');
             $this->elasticaService->getClusterHealth('green', '30s');
+        }
+    }
+
+    private function rebuildEnvironment(Environment $environment, OutputInterface $output): void
+    {
+        if ($environment->getAlias() != $this->instanceId.$environment->getName()) {
+            $environment->setAlias($this->instanceId.$environment->getName());
+            $this->em->persist($environment);
+            $this->em->flush();
+            $output->writeln('Alias has been aligned to '.$environment->getAlias());
+        }
+
+        $contentTypeRepository = $this->em->getRepository(ContentType::class);
+        if (!$contentTypeRepository instanceof ContentTypeRepository) {
+            throw new \RuntimeException('Unexpected ContentTypeRepository object');
+        }
+        $contentTypes = $contentTypeRepository->findAll();
+
+        $body = $this->environmentService->getIndexAnalysisConfiguration();
+
+        $newIndexName = $environment->getNewIndexName();
+        $this->mapping->createIndex($newIndexName, $body);
+
+        $output->writeln('A new index '.$newIndexName.' has been created');
+        $this->waitFor($this->yellowOk, $output);
+        $output->writeln(\count($contentTypes).' content types will be re-indexed');
+
+        $countContentType = 1;
+
+        foreach ($contentTypes as $contentType) {
+            if (!$contentType instanceof ContentType) {
+                throw new \RuntimeException('Unexpected ContentType object');
+            }
+            if (!$contentType->getDeleted() && $contentType->getEnvironment() && $contentType->giveEnvironment()->getManaged()) {
+                $this->contentTypeService->updateMapping($contentType, $newIndexName);
+                $output->writeln('A mapping has been defined for '.$contentType->getSingularName());
+                ++$countContentType;
+            }
+        }
+
+        foreach ($contentTypes as $contentType) {
+            if (!$contentType instanceof ContentType) {
+                throw new \RuntimeException('Unexpected ContentType object');
+            }
+            if (!$contentType->getDeleted() && $contentType->giveEnvironment()->getManaged()) {
+                $this->reindexCommand->reindex($environment->getName(), $contentType, $newIndexName, $output, $this->signData, $this->bulkSize);
+                $output->writeln('');
+                $output->writeln($contentType->getPluralName().' have been re-indexed ');
+            }
+        }
+
+        $this->waitFor($this->yellowOk, $output);
+
+        $atomicSwitch = $this->aliasService->atomicSwitch($environment, $newIndexName);
+
+        foreach ($atomicSwitch as $action) {
+            if (isset($action['add'])) {
+                $output->writeln(\sprintf('The alias <info>%s</info> is now point to : %s', $action['add']['alias'], $action['add']['index']));
+            }
         }
     }
 }

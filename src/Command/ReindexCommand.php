227c41baaea5618101fcffe384fa8f29344faab3
Mathieu De Keyzer <me@theus.be>
2023-11-29 10:56:02 +0100

    refactor: _type is deprecated (#664)

--- src/Command/ReindexCommand.php
+++ src/Command/ReindexCommand.php
@@ -219,7 +219,6 @@ class ReindexCommand extends EmsCommand
             if (isset($item['index']['error'])) {
                 ++$this->error;
                 $this->logger->warning('log.reindex.revision.error', [
-                    EmsFields::LOG_CONTENTTYPE_FIELD => $item['index']['_type'],
                     EmsFields::LOG_OUUID_FIELD => $item['index']['_id'],
                 ]);
             } else {

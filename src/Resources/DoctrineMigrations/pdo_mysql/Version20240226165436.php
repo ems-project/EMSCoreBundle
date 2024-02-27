29bee0545be8740329da0fed96bf6629f497ca38
Mathieu De Keyzer <me@theus.be>
2024-02-27 08:36:17 +0100

    feat(elasticms): add s3 store data and storeDataSessionHandler (#785)
    Co-authored-by: David mattei <d.mattei89@gmail.com>

---
+++ src/Resources/DoctrineMigrations/pdo_mysql/Version20240226165436.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240226165436 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add an expires_at field to StoreData entity';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQLPlatform'."
+        );
+
+        $this->addSql('ALTER TABLE store_data ADD expires_at DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof MySQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\MySQLPlatform'."
+        );
+
+        $this->addSql('ALTER TABLE store_data DROP expires_at');
+    }
+}

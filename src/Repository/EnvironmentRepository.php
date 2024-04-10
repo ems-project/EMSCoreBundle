c37af20e9fd79702120f3efcbdc48654b22a41b5
David mattei <d.mattei89@gmail.com>
2024-04-10 18:25:40 +0200

    chore: phpcs

--- src/Repository/EnvironmentRepository.php
+++ src/Repository/EnvironmentRepository.php
@@ -175,7 +175,7 @@ class EnvironmentRepository extends EntityRepository
     /**
      * @return Environment[]
      */
-    public function get(int $from, int $size, ?string $orderField, string $orderDirection, string $searchValue, ?bool $isManaged = null): array
+    public function get(int $from, int $size, ?string $orderField, string $orderDirection, string $searchValue, bool $isManaged = null): array
     {
         $qb = $this->createQueryBuilder('e')
             ->setFirstResult($from)
@@ -195,7 +195,7 @@ class EnvironmentRepository extends EntityRepository
         return $qb->getQuery()->execute();
     }
 
-    public function counter(string $searchValue = '', ?bool $isManaged = null): int
+    public function counter(string $searchValue = '', bool $isManaged = null): int
     {
         $qb = $this->createQueryBuilder('e');
         $qb->select('count(e.id)');

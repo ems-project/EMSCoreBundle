5137332b7376b9a8bba1b243b3213b9116aa18fd
Mathieu De Keyzer <me@theus.be>
2024-06-04 14:28:30 +0200

    feat(core/twig): add emsco_save_contents twig function (#901)

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -14,6 +14,7 @@ use EMS\CommonBundle\Helper\Text\Encoder;
 use EMS\CommonBundle\Search\Search as CommonSearch;
 use EMS\CommonBundle\Service\ElasticaService;
 use EMS\CommonBundle\Storage\Processor\Config;
+use EMS\CommonBundle\Storage\Service\StorageInterface;
 use EMS\CommonBundle\Twig\AssetRuntime;
 use EMS\CommonBundle\Twig\RequestRuntime;
 use EMS\CoreBundle\Core\ContentType\ContentTypeFields;
@@ -123,6 +124,7 @@ class AppExtension extends AbstractExtension
             new TwigFunction('emsco_get_form', [FormRuntime::class, 'getFormByName']),
             new TwigFunction('emsco_form', [FormRuntime::class, 'handleForm']),
             new TwigFunction('emsco_get_data_field', [FormRuntime::class, 'getDataField']),
+            new TwigFunction('emsco_save_contents', $this->saveContents(...)),
             // deprecated
             new TwigFunction('cant_be_finalized', $this->cantBeFinalized(...), ['deprecated' => true, 'alternative' => 'emsco_cant_be_finalized']),
             new TwigFunction('get_default_environments', [EnvironmentRuntime::class, 'getDefaultEnvironmentNames'], ['deprecated' => true, 'alternative' => 'emsco_get_default_environment_names']),
@@ -256,6 +258,21 @@ class AppExtension extends AbstractExtension
     public function getFile(string $hash): ?string
     {
         return $this->fileService->getFile($hash);
+    }
+
+    /**
+     * @return mixed[]
+     */
+    public function saveContents(string $content, string $filename, string $mimetype, int $usage = StorageInterface::STORAGE_USAGE_ASSET): array
+    {
+        $hash = $this->fileService->saveContents($content, $filename, $mimetype, $usage);
+
+        return [
+            EmsFields::CONTENT_FILE_HASH_FIELD => $hash,
+            EmsFields::CONTENT_FILE_SIZE_FIELD => \strlen($content),
+            EmsFields::CONTENT_FILE_NAME_FIELD => $filename,
+            EmsFields::CONTENT_MIME_TYPE_FIELD => $mimetype,
+        ];
     }
 
     /**

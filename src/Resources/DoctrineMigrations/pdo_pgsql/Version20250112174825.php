8048347570e0203ef79c71c665432a952a4c1c1c
Mathieu De Keyzer <me@theus.be>
2025-01-12 19:15:38 +0100

    refactor(admin): remove radio and select fields (converted to choice) (#1134)

---
+++ src/Resources/DoctrineMigrations/pdo_pgsql/Version20250112174825.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+use EMS\CoreBundle\Entity\FieldType;
+use EMS\CoreBundle\Form\DataField\ChoiceFieldType;
+use EMS\Helpers\Standard\Json;
+
+final class Version20250112174825 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Replace RadioFieldType and SelectFieldType by ChoiceFieldType';
+    }
+
+    #[\Override]
+    public function up(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof PostgreSQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQLPlatform'."
+        );
+        $fieldTypes = $this->connection->executeQuery('select id, type, options from field_type');
+        while ($fieldType = $fieldTypes->fetchAssociative()) {
+            $options = Json::decode($fieldType['options']);
+            if ('EMS\CoreBundle\Form\DataField\RadioFieldType' === $fieldType['type']) {
+                $options[FieldType::DISPLAY_OPTIONS]['expanded'] = true;
+                $options[FieldType::DISPLAY_OPTIONS]['multiple'] = false;
+            } elseif ('EMS\CoreBundle\Form\DataField\SelectFieldType' === $fieldType['type']) {
+                $options[FieldType::DISPLAY_OPTIONS]['expanded'] = false;
+                $options[FieldType::DISPLAY_OPTIONS]['multiple'] = $options[FieldType::DISPLAY_OPTIONS]['multiple'] ?? false;
+            } else {
+                continue;
+            }
+            $this->addSql('UPDATE field_type SET options = :options WHERE id = :id', [
+                'id' => $fieldType['id'],
+                'type' => ChoiceFieldType::class,
+                'options' => Json::encode($options),
+            ]);
+        }
+    }
+
+    #[\Override]
+    public function down(Schema $schema): void
+    {
+        $this->abortIf(
+            !$this->connection->getDatabasePlatform() instanceof PostgreSQLPlatform,
+            "Migration can only be executed safely on '\Doctrine\DBAL\Platforms\PostgreSQLPlatform'."
+        );
+    }
+}

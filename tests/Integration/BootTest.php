41fe90cd587820a6f6214cd72fa766a9ef73dffc
David mattei <d.mattei89@gmail.com>
2025-01-25 09:01:22 +0100

    chore: fix last phpUnit deprecations (doctrine) (#1157)

---
+++ tests/Integration/BootTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EMS\CoreBundle\Tests\Integration;
+
+use EMS\CoreBundle\Tests\Integration\App\Kernel;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+final class BootTest extends KernelTestCase
+{
+    public function testKernelIsBooted()
+    {
+        self::bootKernel();
+        $this->assertTrue(self::$booted);
+    }
+
+    protected static function getKernelClass(): string
+    {
+        return Kernel::class;
+    }
+}

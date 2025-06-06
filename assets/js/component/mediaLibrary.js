9c7221d98271fae9e35bca2f2d6bbdd7295bc513
Mathieu De Keyzer <me@theus.be>
2025-06-06 11:42:13 +0200

    feat(admin/media-lib): add behavior for view file on click (#1342)

--- assets/js/component/mediaLibrary.js
+++ assets/js/component/mediaLibrary.js
@@ -104,7 +104,7 @@ export default class MediaLibrary {
             if (classList.contains('media-lib-folder')) this._onClickFolder(event.target);
 
             if (classList.contains('btn-file-upload')) this.#elements.inputUpload.click();
-            if (classList.contains('btn-file-view')) this._onClickButtonFileView(event.target);
+            if (classList.contains('btn-file-view')) this._onClickButtonFileView(event.target, event);
             if (classList.contains('btn-file-rename')) this._onClickButtonFileRename(event.target);
             if (classList.contains('btn-file-delete')) this._onClickButtonFileDelete(event.target);
             if (classList.contains('btn-files-delete')) this._onClickButtonFilesDelete(event.target)
@@ -152,7 +152,8 @@ export default class MediaLibrary {
 
         this._getFiles().then(() => this.loading(false));
     }
-    _onClickButtonFileView(button) {
+  _onClickButtonFileView(button, event) {
+        event.preventDefault()
         const getSiblingFile = (fileId, sibling) => {
             const row = this.#elements.listFiles.querySelector(`.media-lib-file[data-id='${fileId}']`);
             const rowSibling = row.closest('li')[sibling];
@@ -164,6 +165,7 @@ export default class MediaLibrary {
             if (!button || null === getSiblingFile(fileId, sibling)) return;
 
             button.style.display = 'inline-block';
+            button.classList.remove('disabled')
             button.addEventListener('click', () => {
                 const file = getSiblingFile(fileId, sibling);
                 if (!file) return;

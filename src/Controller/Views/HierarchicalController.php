6d14d8203eb7cca858ec533a2c90bd32f77f9a0d
David mattei <d.mattei89@gmail.com>
2025-11-17 16:19:58 +0100

    fix(admin/view): hierarchical reorder clears children (#1465)

--- src/Controller/Views/HierarchicalController.php
+++ src/Controller/Views/HierarchicalController.php
@@ -14,17 +14,18 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class HierarchicalController extends AbstractController
 {
-    public function __construct(private readonly ContentTypeService $contentTypeService, private readonly SearchService $searchService, private readonly string $templateNamespace)
-    {
+    public function __construct(
+        private readonly ContentTypeService $contentTypeService,
+        private readonly SearchService $searchService,
+        private readonly string $templateNamespace
+    ) {
     }
 
     public function item(View $view, string $key): Response
     {
         $ouuid = \explode(':', $key);
-        $contentType = $this->contentTypeService->getByName($ouuid[0]);
-        if (false === $contentType) {
-            throw new NotFoundHttpException(\sprintf('Content type %s not found', $ouuid[0]));
-        }
+        $contentType = $this->contentTypeService->giveByName($ouuid[0]);
+
         try {
             $document = $this->searchService->getDocument($contentType, $ouuid[1]);
         } catch (NotFoundException) {
@@ -32,7 +33,7 @@ class HierarchicalController extends AbstractController
         }
 
         return $this->render("@$this->templateNamespace/view/custom/hierarchical_add_item.html.twig", [
-            'data' => $document->getSource(),
+            'data' => $document,
             'view' => $view,
             'contentType' => $contentType,
             'key' => $ouuid,

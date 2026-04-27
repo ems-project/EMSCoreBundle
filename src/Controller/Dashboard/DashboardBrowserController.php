95681301dda9fa30d994a53614969569dd7b433a
Mathieu De Keyzer <me@theus.be>
2026-04-27 11:17:09 +0200

    chore: ongoing

--- src/Controller/Dashboard/DashboardBrowserController.php
+++ src/Controller/Dashboard/DashboardBrowserController.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace EMS\CoreBundle\Controller\Dashboard;
 
 use EMS\CoreBundle\Core\Dashboard\DashboardManager;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 
@@ -17,23 +19,32 @@ class DashboardBrowserController
     ) {
     }
 
-    public function __invoke(string $dashboardName): Response
+    public function __invoke(string $dashboardName, Request $request): Response
     {
+        $format = $request->query->get('format', 'html');
         $dashboard = $this->dashboardManager->getByName($dashboardName);
-        $response = new Response();
 
         try {
-            $response->setContent($this->twig->render("@$this->templateNamespace/dashboard/browser/dashboard-browser-modal.html.twig", [
+            $content = $this->twig->render("@$this->templateNamespace/dashboard/browser/dashboard-browser-modal.$format.twig", [
                 'dashboard' => $dashboard,
-            ]));
+            ]);
+            $success = true;
         } catch (\Throwable $e) {
-            $response->setContent($this->twig->render("@$this->templateNamespace/dashboard/browser/dashboard-browser-modal-error.html.twig", [
+            $content = $this->twig->render("@$this->templateNamespace/dashboard/browser/dashboard-browser-modal-error.$format.twig", [
                 'exception' => $e,
                 'dashboard' => $dashboard,
-            ]));
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+            ]);
+            $success = false;
         }
 
-        return $response;
+        if ($format === 'json') {
+            dump('json');
+            return new JsonResponse([
+                'success' => $success,
+                'content' => $content,
+            ], $success ? Response::HTTP_OK : Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return new Response($content, $success ? Response::HTTP_OK : Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }

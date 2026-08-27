5100f14167683091a332fb442533bc5fc4beeaab
David mattei <d.mattei89@gmail.com>
2026-08-27 14:19:02 +0200

    feat(admin/job): add EMSCO_JOB_CLEAN_TIME environment variable (#1814)

--- src/Command/JobCommand.php
+++ src/Command/JobCommand.php
@@ -19,8 +19,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[AsCommand(
     name: Commands::JOB_RUN,
     description: 'Execute the next pending job if exists. If not execute the oldest due scheduled job if exists.',
-    hidden: false,
-    aliases: ['ems:job:run']
+    aliases: ['ems:job:run'],
+    hidden: false
 )]
 class JobCommand extends AbstractCommand
 {
@@ -38,7 +38,6 @@ class JobCommand extends AbstractCommand
         private readonly ReleaseService $releaseService,
         private readonly RunnerManager $runnerManager,
         private readonly string $dateFormat,
-        private readonly string $cleanJobsTimeString,
     ) {
         parent::__construct();
     }
@@ -72,7 +71,10 @@ class JobCommand extends AbstractCommand
         }
 
         $this->io->comment('Nothing to run. Cleaning jobs.');
-        $this->jobService->cleanJob(self::USER_JOB_COMMAND, $this->cleanJobsTimeString);
+        $this->jobService->clean(
+            skipFailed: true,
+            includeJobTime: true
+        );
 
         return self::EXECUTE_SUCCESS;
     }
--- src/Service/JobService.php
+++ src/Service/JobService.php
@@ -42,6 +42,7 @@ class JobService implements EntityServiceInterface
         private readonly TokenStorageInterface $tokenStorage,
         private readonly MessageBusInterface $bus,
         private readonly bool $asyncEnabled,
+        private readonly string $cleanJobTime,
     ) {
         $this->em = $doctrine->getManager();
     }
@@ -92,16 +93,6 @@ class JobService implements EntityServiceInterface
         $this->save($job);
 
         return $job;
-    }
-
-    public function clean(): void
-    {
-        $doneJobs = $this->repository->findBy(['done' => true]);
-        foreach ($doneJobs as $doneJob) {
-            $this->em->remove($doneJob);
-        }
-
-        $this->em->flush();
     }
 
     /**
@@ -250,17 +241,22 @@ class JobService implements EntityServiceInterface
         return $job;
     }
 
-    public function cleanJob(string $username, string $stringTime): int
+    public function clean(bool $skipFailed, bool $includeJobTime = false): int
     {
-        try {
-            $olderDate = DateTime::create($stringTime);
-        } catch (\Throwable $e) {
-            $this->logger->warning(\sprintf('Invalid string to time format: %s (%s)', $stringTime, $e->getMessage()));
+        $modifiedBefore = null;
 
-            return 0;
+        if ($includeJobTime) {
+            try {
+                $modifiedBefore = DateTime::create($this->cleanJobTime);
+            } catch (\Throwable $e) {
+                $this->logger->warning(\sprintf('Invalid string to time format: %s (%s)', $this->cleanJobTime, $e->getMessage()));
+
+                return 0;
+            }
         }
+
         try {
-            $jobsCleaned = $this->repository->clean($username, $olderDate);
+            $jobsCleaned = $this->repository->clean($skipFailed, $modifiedBefore);
         } catch (\Throwable $e) {
             $this->logger->warning(\sprintf('Error during cleaning jobs: %s', $e->getMessage()));
 
@@ -268,7 +264,7 @@ class JobService implements EntityServiceInterface
         }
 
         if ($jobsCleaned > 0) {
-            $this->logger->notice(\sprintf('%d scheduled jobs have been cleaned', $jobsCleaned));
+            $this->logger->notice(\sprintf('%d jobs have been cleaned', $jobsCleaned));
         }
 
         return $jobsCleaned;
